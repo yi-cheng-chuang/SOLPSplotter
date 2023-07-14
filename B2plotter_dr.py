88ef8109cbadb8ef71df33d807ebec3cc28c8935
yctok <ychuang@wm.edu>
2023-07-14 01:42:43 -0400

    Update B2plotter_dr.py

--- B2plotter_dr.py
+++ B2plotter_dr.py
@@ -5,12 +5,23 @@ Created on Thu Jul 13 12:45:19 2023
 @author: user
 """
 import B2plotter_set as b2s
-import New_plotter as b2p
+import B2plotter_class as b2c
 
 d = b2s.Setting_dic()
 
 
-xp = b2p.B2plotter(Shot= d['Shot'],Attempt= d['Attempt'], 
-                   Parameters= d['Parameters'], 
-                   DefaultSettings= d['DefaultSettings']).DefaultSettings
-print(xp)
+# xd = b2c.B2plotter(DEV= d['DEV'], Shot= d['Shot'], shift= d['shift'], 
+#                    series= d['series'], Attempts= d['Attempts'], 
+#                    Parameters= d['Parameters'], 
+#                    DefaultSettings= d['DefaultSettings'])
+
+
+xp = b2c.mastdata(DEV= d['DEV'], Shot= d['Shot'], shift= d['shift'], 
+                    series= d['series'], Attempts= d['Attempts'], 
+                    Parameters= d['Parameters'], loadDS= d['loadDS'], 
+                    DefaultSettings= d['DefaultSettings'])
+# print(type(xd.Parameters))
+xp.loadmastdata(a_shift='org')
+xp.mastcalcpsi(a_shift='org', geo=None, b2mn=None, dsa=None, shift= 0)
+xpp = xp.data['dirdic']['fitloc']
+print(xpp)
