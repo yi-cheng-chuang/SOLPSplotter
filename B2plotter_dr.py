ea0ac0b2eb535da13b9be71e26437b1ab258cfab
yctok <ychuang@wm.edu>
2023-07-31 17:02:37 -0400

    Update B2plotter_dr.py

--- B2plotter_dr.py
+++ B2plotter_dr.py
@@ -6,11 +6,12 @@ Created on Thu Jul 13 12:45:19 2023
 """
 import B2plotter_set as b2s
 import B2plotter_class as b2c
+import B2plotter_plot as b2p
 
 d = b2s.Setting_dic()
 lex = b2s.loadDS_dic(d['DEV'])
 
-xl = b2c.simple_plot(DEV = d['DEV'], withshift= d['withshift'], DefaultSettings = d['DefaultSettings'],
+xl = b2p.simple_plot(DEV = d['DEV'], withshift= d['withshift'], DefaultSettings = d['DefaultSettings'],
                    loadDS = lex, Parameters= d['Parameters'], 
                    Publish= d['Publish'])
 xl.load_mast_dir()
@@ -20,7 +21,7 @@ xl.calcpsi_1D(pol_loc= '57')
 # xl.load_vessel()
 # gd = xl.creat_grid()
 # xl.load_output_geo(grid_dic= gd)
-xl.plot_Ne_NeuDen(pol_loc= '57')
+xl.plot_Ne_NeuDen_test(pol_loc= '57')
 # p = xl.data['dircomp']['Attempt']
 q = xl.data['gfile']['gcomp']['check']
 # print(p)
