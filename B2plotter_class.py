fd7d5e7ba63f43765b801bc9b5404a7f4ae76ddd
yctok <ychuang@wm.edu>
2023-07-31 17:02:34 -0400

    Update B2plotter_class.py

--- B2plotter_class.py
+++ B2plotter_class.py
@@ -10,10 +10,8 @@ import numpy as np
 import xarray as xr
 import glob
 from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 from matplotlib import colors, cm
 from scipy.io import loadmat
-from scipy import interpolate
 from D3DPreProcess import PsiNtoR
 import equilibrium as eq
 from D3DPreProcess import RhotoPsiN
@@ -194,7 +192,7 @@ class B2plotter:
             interp_dic = {}
             for ab in self.data['dircomp']['multi_shift']:
                 shift = self.data['dircomp']['shiftdic'][ab]
-                print(shift)
+                # print(shift)
         
                 gR = np.zeros(g['nw'])
                 for i in range(g['nw']):
@@ -225,9 +223,7 @@ class B2plotter:
     
     
     def calcpsi(self):
-        # for j in range(len(Y)):
-        #     for i in range(len(X)):
-        #         PsinLoc.values[j,i,n] = GF.psiN(RadLoc.loc[Y[j],X[i],Attempt].values,VertLoc.loc[Y[j],X[i],Attempt].values,)
+    
         
         geo = self.data['b2fgeo']
         pol_range = int(self.data['b2fgeo']['nx'] + 2)
@@ -290,7 +286,7 @@ class B2plotter:
             
             
             psiNinterp_RGI, psiNinterp_2d, psiNinterp_RBS = self.load_solpsgeo()
-            print(type(psiNinterp_RBS))
+            # print(type(psiNinterp_RBS))
             psival = np.zeros((pol_range, rad_range))
             
             pol_index = int(pol_loc) + 1
@@ -312,7 +308,7 @@ class B2plotter:
             URsep = np.mean([crUpperRight[int(SEP)-1], crUpperRight[int(SEP)-2]])
             
             weight = dsa[int(SEP)-1]/ (dsa[int(SEP)-1] - dsa[int(SEP)-2])
-            print(weight)
+            # print(weight)
             
             LLdsa = crLowerLeft - LLsep
             LRdsa = crLowerRight - LRsep
@@ -373,7 +369,7 @@ class B2plotter:
                                             psi_LR_RBS, psi_UR_RBS])
             
             GF = eq.equilibrium(gfile= self.data['dirdata']['gdir'][0])
-            print(type(GF.psiN._func))
+            # print(type(GF.psiN._func))
             psi_solps_GF = np.zeros(rad_range)
             for i in range(rad_range):
                 psi_LL_GF = GF.psiN(crLowerLeft[i], czLowerLeft[i])
@@ -976,287 +972,8 @@ class load_data(load_expdata):
             print('There is a bug')
             
             
-class simple_plot(load_data):
-    def __init__(self, DEV, withshift, DefaultSettings, loadDS, Parameters, Publish):
-        load_data.__init__(self, DEV, withshift, DefaultSettings, loadDS, Parameters)
-        
-        self.Publish = Publish
-        self.data['DefaultSettings']['Publish'] = self.Publish
-    
-    def set_plot(self):
-        if self.Publish == 'b2plottersetting':
-            plt.rcParams.update({'font.weight': 'bold'})
-            plt.rcParams.update({'font.size': 16})
-            plt.rcParams.update({'figure.facecolor':'w'})
-            plt.rcParams.update({'mathtext.default': 'regular'})
-            # if Publish != []:
-            #     plt.rc('font',size=30)
-            #     plt.rc('lines',linewidth=5,markersize=15)
-            # else:
-            #     plt.rc('font',size=14)
-        else:
-            print('Publish setting is incorrect or add another setting')
-    
-
-    
-    def plot_Ne_NeuDen(self, pol_loc):
-        if self.withshift == False:
-            self.load_output_data(param= 'NeuDen')
-            self.load_output_data(param= 'Ne')
-            self.load_output_data(param= 'Te')
-            
-            psi = self.data['psi']['psi_{}_val'.format(pol_loc)][:, 2]
-            dsa_pol_loc = self.data['psi']['dsa_{}_val'.format(pol_loc)]
-            # psi_RGI = self.data['psi']['psi_{}_val'.format(pol_loc)][:, 0]
-            
-            pol_index = int(pol_loc) + 1
-            Nd = self.data['outputdata']['NeuDen'][:, pol_index]
-            Ne = self.data['outputdata']['Ne'][:, pol_index]
-            Te = self.data['outputdata']['Te'][:, pol_index]
-            
-            self.loadmastdata()
-            fitpsi = self.data['fitprofile']['psi_normal']
-            fitNe = self.data['fitprofile']['electron_density(m^(-3))']
-            
-            psi_to_dsa_func = interpolate.interp1d(psi, dsa_pol_loc, fill_value = 'extrapolate')
-                  
-            cutpsi = []
-            cutNe = []
-            n = len(fitpsi)
-            for i in range(n):
-                if fitpsi[i] >= min(psi):
-                    cutpsi.append(fitpsi[i])
-                    cutNe.append(fitNe[i])
-            
-            cutpsi = np.asarray(cutpsi)
-            cutNe = np.asarray(cutNe)
-            
-            fitdsa = psi_to_dsa_func(cutpsi)
-            
-            dsa_cut = []
-            an_cut = []
-            for j in range(len(dsa_pol_loc)):
-                if dsa_pol_loc[j] <= 0:
-                    dsa_cut.append(dsa_pol_loc[j])
-                    an_cut.append(Nd[j])
-            
-            dsa_cut = np.asarray(dsa_cut)
-            an_cut = np.asarray(an_cut)
-            
-            norm_an = an_cut/ max(an_cut)
-            
-            pn = [1, 250]
-            
-            popt_an, pcov_an = curve_fit(fm.expfit, dsa_cut, norm_an, pn)
-            print(popt_an)
-            
-            exp_an_fit = fm.expfit(dsa_cut, popt_an[0]*max(an_cut), popt_an[1])
-            
-            efold = 1/popt_an[1]
-            
-            
-            fit_dic = fm.tanh_dsa_fit(dsa_pol_loc, Ne, Te)
-            tanh_ne_fit = fit_dic['tanh_ne_fit']*pow(10, 20)
-            dn = fit_dic['popt_ne'][1]
-            
-            
-            opq = 2*dn/efold
-            print(opq)
-            
-            x = [-efold + max(dsa_cut), max(dsa_cut)]
-            y = [max(exp_an_fit), max(exp_an_fit)]
-            xd = [-dn, dn]
-            yd = [cutNe[109], cutNe[109]]
-            
-            
-            plt.figure(1)
-            plt.plot(dsa_pol_loc, Nd,'o-', color = 'green', label= 'solps neutral density')
-            # plt.plot(psi_RGI, Nd,'o-', color = 'b', label= 'RGI_solps neutral density')
-            plt.plot(dsa_cut, exp_an_fit, color='r',lw= 5, label= 'exponential fit')
-            plt.axvline(x=max(dsa_cut), color='orange',lw=3)
-            plt.plot(x,y, color='orange', lw=3, label= 'Neutral penetration length [m]: $\lambda_{n_D}$')
-            plt.axvline(x=-efold + max(dsa_cut), color='orange',lw=3)
-            plt.axvline(x=dn, color='black',lw=3, ls='--')
-            plt.axvline(x=-dn, color='black',lw=3, ls='--')
-            plt.text(0.1, 5*pow(10, 16), 'dimensionless opaqueness: {}'.format(opq))
-            plt.xlabel('Radial coordinate: $R- R_{sep}$')
-            plt.ylabel(self.data['Parameter']['NeuDen'])
-            plt.title('Neutral density with fits')
-            # plt.title(plot_dic['an3da.last10'][0],fontdict={"family":"Calibri","size": 20})
-            plt.legend()
-                
-            # plt.subplot(211, sharex= ax1)
-            plt.figure(2)
-            # plt.plot(psi_xport, Ne,'o-', color = 'r', label= 'solps_electron density')
-            plt.plot(dsa_pol_loc, Ne,'o-', color = 'b', label= 'solps electron density')
-            # plt.plot(fitdsa, cutNe,'o-', color = 'g', label= 'experiment electron density')
-            plt.plot(dsa_pol_loc, tanh_ne_fit, color='r',lw= 3, label= 'exponential fit')
-            plt.plot(xd, yd, color='black', lw=3, label= 'Pedestal width [m]: $\Delta n_e$')
-            plt.axvline(x=dn, color='black',lw=3)
-            plt.axvline(x=-dn, color='black',lw=3)
-            plt.axvline(x=0, color='orange',lw=3, ls='--')
-            plt.axvline(x=-efold, color='orange',lw=3, ls='--')
-            plt.xlabel('Radial coordinate: $R- R_{sep}$')
-            plt.ylabel(self.data['Parameter']['Ne'])
-            plt.title('Electron density with fits')
-            # plt.title(plot_dic['ne3da.last10'][0],fontdict={"family":"Calibri","size": 20})
-            plt.legend()
-            
-            plt.show()
-            
-            return opq
-        
-        elif self.withshift == True:
-            self.load_output_data(param= 'NeuDen')
-            self.load_output_data(param= 'Ne')
-            self.load_output_data(param= 'Te')
-            
-            efold_dic = {}
-            delta_dic = {}
-            opq_dic = {}
-            
-            dsa_pol_loc_dic = {}
-            dsa_cut_dic = {}
-            Nd_dic = {}
-            Ne_dic = {}
-            Te_dic = {}
-            exp_an_fit_dic = {}
-            tanh_ne_fit_dic = {}
-            
-            
-            for aa in self.data['dircomp']['multi_shift']:
-                psi = self.data['psi']['psi_{}_val'.format(pol_loc)][aa][:, 2]
-                dsa_pol_loc_dic[aa] = self.data['psi']['dsa_{}_val'.format(pol_loc)][aa]
-                # psi_RGI = self.data['psi']['psi_{}_val'.format(pol_loc)][:, 0]
-                
-                pol_index = int(pol_loc) + 1
-                Nd_dic[aa] = self.data['outputdata']['NeuDen'][aa][:, pol_index]
-                Ne_dic[aa] = self.data['outputdata']['Ne'][aa][:, pol_index]
-                Te_dic[aa] = self.data['outputdata']['Te'][aa][:, pol_index]
-            
-            # self.loadmastdata()
-            # fitpsi = self.data['fitprofile']['psi_normal']
-            # fitNe = self.data['fitprofile']['electron_density(m^(-3))']
-            
-                # psi_to_dsa_func = interpolate.interp1d(psi, dsa_pol_loc, fill_value = 'extrapolate')
-                  
-                # cutpsi = []
-                # cutNe = []
-                # n = len(fitpsi)
-                # for i in range(n):
-                #     if fitpsi[i] >= min(psi):
-                #         cutpsi.append(fitpsi[i])
-                #         cutNe.append(fitNe[i])
-                
-                # cutpsi = np.asarray(cutpsi)
-                # cutNe = np.asarray(cutNe)
-                
-                # fitdsa = psi_to_dsa_func(cutpsi)
-                
-                dsa_cut = []
-                an_cut = []
-                for j in range(len(dsa_pol_loc_dic[aa])):
-                    if dsa_pol_loc_dic[aa][j] <= 0:
-                        dsa_cut.append(dsa_pol_loc_dic[aa][j])
-                        an_cut.append(Nd_dic[aa][j])
-                
-                dsa_cut_dic[aa] = np.asarray(dsa_cut)
-                an_cut = np.asarray(an_cut)
-                
-                norm_an = an_cut/ max(an_cut)
-                
-                pn = [1, 250]
-                
-                popt_an, pcov_an = curve_fit(fm.expfit, dsa_cut, norm_an, pn)
-                print(popt_an)
-                
-                exp_an_fit_dic[aa] = fm.expfit(dsa_cut_dic[aa], popt_an[0]*max(an_cut), popt_an[1])
-                
-                efold_dic[aa] = 1/popt_an[1]
-                
-                
-                fit_dic = fm.tanh_dsa_fit(dsa_pol_loc_dic[aa], Ne_dic[aa], Te_dic[aa])
-                tanh_ne_fit_dic[aa] = fit_dic['tanh_ne_fit']*pow(10, 20)
-                delta_dic[aa] = fit_dic['popt_ne'][1]
-                
-                
-                opq_dic[aa] = 2*delta_dic[aa]/efold_dic[aa]
-                # print(opq)
-            
-            
-            plt.figure(1)
-            for i in self.data['dircomp']['multi_shift']:
-                plt.plot(dsa_pol_loc_dic[i], Nd_dic[i],'o-', label= 'solps neutral density_{}'.format(i))
-                # plt.plot(psi_RGI, Nd,'o-', color = 'b', label= 'RGI_solps neutral density')
-                plt.plot(dsa_cut_dic[i], exp_an_fit_dic[i], lw= 5, label= 'exponential fit_{}'.format(i))
-            # plt.axvline(x=max(dsa_cut), color='orange',lw=3)
-            # plt.plot(x,y, color='orange', lw=3, label= 'Neutral penetration length [m]: $\lambda_{n_D}$')
-            # plt.axvline(x=-efold + max(dsa_cut), color='orange',lw=3)
-            # plt.axvline(x=dn, color='black',lw=3, ls='--')
-            # plt.axvline(x=-dn, color='black',lw=3, ls='--')
-            # plt.text(0.1, 5*pow(10, 16), 'dimensionless opaqueness: {}'.format(opq))
-            plt.xlabel('Radial coordinate: $R- R_{sep}$')
-            plt.ylabel(self.data['Parameter']['NeuDen'])
-            plt.title('Neutral density with fits')
-            # plt.title(plot_dic['an3da.last10'][0],fontdict={"family":"Calibri","size": 20})
-            plt.legend()
-                
-            # plt.subplot(211, sharex= ax1)
-            plt.figure(2)
-            # plt.plot(psi_xport, Ne,'o-', color = 'r', label= 'solps_electron density')
-            for j in self.data['dircomp']['multi_shift']:
-                plt.plot(dsa_pol_loc_dic[i], Ne_dic[i],'o-', label= 'solps electron density_{}'.format(i))
-            # plt.plot(fitdsa, cutNe,'o-', color = 'g', label= 'experiment electron density')
-            # plt.plot(dsa_pol_loc_dic[i], tanh_ne_fit, color='r',lw= 3, label= 'exponential fit')
-            # plt.plot(xd, yd, color='black', lw=3, label= 'Pedestal width [m]: $\Delta n_e$')
-            # plt.axvline(x=dn, color='black',lw=3)
-            # plt.axvline(x=-dn, color='black',lw=3)
-            # plt.axvline(x=0, color='orange',lw=3, ls='--')
-            # plt.axvline(x=-efold, color='orange',lw=3, ls='--')
-            plt.xlabel('Radial coordinate: $R- R_{sep}$')
-            plt.ylabel(self.data['Parameter']['Ne'])
-            plt.title('Electron density with fits')
-            # plt.title(plot_dic['ne3da.last10'][0],fontdict={"family":"Calibri","size": 20})
-            plt.legend()
-            
-            
-            shift_ar = {}
-            for k in self.data['dircomp']['multi_shift']:
-                kk = float(self.data['dircomp']['shiftdic'][k])
-                shift_ar[k] = kk
-            
-            plt.figure(3)
-            # plt.plot(psi_xport, Ne,'o-', color = 'r', label= 'solps_electron density')
-            for j in self.data['dircomp']['multi_shift']:
-                plt.plot(shift_ar[j], opq_dic[j],'o-', color= 'r', label= 'dimensionless opaqueness')
-            # plt.plot(fitdsa, cutNe,'o-', color = 'g', label= 'experiment electron density')
-            # plt.plot(dsa_pol_loc_dic[i], tanh_ne_fit, color='r',lw= 3, label= 'exponential fit')
-            # plt.plot(xd, yd, color='black', lw=3, label= 'Pedestal width [m]: $\Delta n_e$')
-            # plt.axvline(x=dn, color='black',lw=3)
-            # plt.axvline(x=-dn, color='black',lw=3)
-            # plt.axvline(x=0, color='orange',lw=3, ls='--')
-            # plt.axvline(x=-efold, color='orange',lw=3, ls='--')
-            plt.xlabel('shift: [m]')
-            plt.title('opaqueness verses shift distance')
-            # plt.title(plot_dic['ne3da.last10'][0],fontdict={"family":"Calibri","size": 20})
-        else:
-            print('We can go to sleep')
 
 
-    
-
-        
-        
-    
-    
-    
-
-            
-            
-            
-    
-        
-        
         
             
             
