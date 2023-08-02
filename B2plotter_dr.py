8ccb814dcbf874f59139b5fa78570dad77d0b313
yctok <ychuang@wm.edu>
2023-08-01 22:58:14 -0400

    Update B2plotter_dr.py

--- B2plotter_dr.py
+++ B2plotter_dr.py
@@ -11,21 +11,22 @@ import B2plotter_plot as b2p
 d = b2s.Setting_dic()
 lex = b2s.loadDS_dic(d['DEV'])
 
-xl = b2p.simple_plot(DEV = d['DEV'], withshift= d['withshift'], DefaultSettings = d['DefaultSettings'],
-                   loadDS = lex, Parameters= d['Parameters'], 
-                   Publish= d['Publish'])
+xl = b2p.simple_plot(DEV = d['DEV'], withshift= d['withshift'], withseries= d['withseries'],
+            DefaultSettings = d['DefaultSettings'], loadDS = lex, 
+            Parameters= d['Parameters'], Publish= d['Publish'])
 xl.load_mast_dir()
-xl.load_solpsgeo()
+# xl.load_solpsgeo()
 # xl.calcpsi()
-xl.calcpsi_1D(pol_loc= '57')
+# xl.calcpsi_1D(pol_loc= '57')
 # xl.load_vessel()
 # gd = xl.creat_grid()
 # xl.load_output_geo(grid_dic= gd)
-xl.plot_Ne_NeuDen_withshift(pol_loc= '57')
+# xl.plot_Ne_NeuDen_single(pol_loc= '57')
+# xl.plot_Ne_NeuDen_withshift(pol_loc= '57')
 # p = xl.data['dircomp']['Attempt']
-q = xl.data['gfile']['gcomp']['check']
+# q = xl.data['gfile']['gcomp']['check']
 # print(p)
-print(q)
+# print(q)
 
 
 
