5928c972cb78f89a74f45f7e2dae1cb4b9e31012
yctok <ychuang@wm.edu>
2023-09-26 18:01:11 -0400

    Update B2plotter_dr.py

--- B2plotter_dr.py
+++ B2plotter_dr.py
@@ -5,8 +5,9 @@ Created on Thu Jul 13 12:45:19 2023
 @author: user
 """
 import B2plotter_set as b2s
-import B2plotter_class as b2c
+import SOLPSplotter_geoRR as sgr
 import B2plotter_plot as b2p
+import B2plotter_mid_calc as b2mc
 
 d = b2s.Setting_dic()
 lex = b2s.loadDS_dic(d['DEV'])
@@ -14,26 +15,63 @@ lex = b2s.loadDS_dic(d['DEV'])
 
 
 
-xl = b2p.simple_plot(DEV = d['DEV'], withshift= d['withshift'], withseries= d['withseries'],
+xl = b2p.Opacity_study(DEV = d['DEV'], withshift= d['withshift'], withseries= d['withseries'],
             DefaultSettings = d['DefaultSettings'], loadDS = lex, 
             Parameters= d['Parameters'], Publish= d['Publish'])
 xl.load_mast_dir()
 xl.load_solpsgeo()
+# xl.calc_RRsep()
 # xl.calcpsi()
-# xl.calcpsi_1D(pol_loc= '40')
+# xl.calc_RRsep()
+xl.calcpsi_1D(pol_loc= '35')
+# xl.calc_flux_expansion(polpos= '55')
 # xl.load_vessel()
-# gd = xl.creat_grid()
-# xl.load_output_geo(grid_dic= gd)
+
 xl.set_plot()
-# xl.Opacity_study_radial_plot_psi(pol_loc= '40')
-poloidal_index_list = []
-for i in range(45):
-    poloidal_index_list.append('{}'.format(26 + i))
-xl.Opacity_study_poloidal_plot(pol_list= poloidal_index_list)
+# xl.Opacity_study_radial_plot_dsa(pol_loc= '57')
+xl.Opacity_study_radial_plot(pol_loc= '35', x_choice= 'psiN')
+
+
+# poloidal_index_list = []
+# for i in range(34):
+#     poloidal_index_list.append('{}'.format(36 + i))
+# xl.Opacity_study_poloidal_plot(pol_list= poloidal_index_list, x_choice= 'psiN')
+
+
 # p = xl.data['dircomp']['Attempt']
 # q = xl.data['gfile']['gcomp']['check']
 # print(p)
 # print(q)
+
+
+
+
+"Use functions Richard.R created"
+# xd = sgr.geo_RR(DEV = d['DEV'], withshift= d['withshift'], withseries= d['withseries'],
+#             DefaultSettings = d['DefaultSettings'])
+# xd.load_mast_dir()
+# xd.load_solpsgeo()
+# gd = xd.creat_grid()
+# xd.load_output_geo(grid_dic= gd)
+# xd.dsa_1D(pol_loc= '46')
+
+"Use functions for mid_plane calculation"
+# xt = b2mc.psi_RRsep_calc(DEV = d['DEV'], withshift= d['withshift'], withseries= d['withseries'],
+#             DefaultSettings = d['DefaultSettings'])
+
+# xt.load_mast_dir()
+# xt.load_solpsgeo()
+# xt.calcpsi()
+# # xt.plot_psi_surface()
+# xt.plot_seperatrix()
+
+# poloidal_index_list = []
+# for i in range(30):
+#     poloidal_index_list.append('{}'.format(30 + i))
+# xt.plot_psi(pol_list= poloidal_index_list)
+# xt.plot_RZ(pol_list= poloidal_index_list)
+
+
 
 
 
