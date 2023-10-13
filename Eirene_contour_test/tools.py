7b7c76ed8d88e13052efb5eb53d08531fcd69b20
yctok <ychuang@wm.edu>
2023-10-12 22:50:57 -0400

    Update tools.py

--- Eirene_contour_test/tools.py
+++ Eirene_contour_test/tools.py
@@ -63,7 +63,7 @@ def mast_dir_dic():
     'dev': 'mast',
     'shot': '027205',
     'shift': ['org_new_series', 'dot3', 'dot5', 'dot7', 'one_LS'],
-    'series' : ['39_noc_nts5_a', '1_n5_dot3_a', '15_dn0.5hc0.05_ts5_dot5_a', '8_t1_dot7_a', '27_lsts5_tw_one_a']
+    'series' : ['42_tar911_nts5_a', '1_n5_dot3_a', '15_dn0.5hc0.05_ts5_dot5_a', '8_t1_dot7_a', '27_lsts5_tw_one_a']
     }
     return mast_dir_dic
 
