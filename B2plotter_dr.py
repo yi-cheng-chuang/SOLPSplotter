9179a0ff4cc4edf9b40996c51d1b6e294a07b973
yctok <ychuang@wm.edu>
2023-11-26 18:25:59 -0500

    Update B2plotter_dr.py

--- B2plotter_dr.py
+++ B2plotter_dr.py
@@ -7,6 +7,7 @@ Created on Thu Jul 13 12:45:19 2023
 import B2plotter_set as b2s
 import SOLPSplotter_geoRR as sgr
 import B2plotter_plot as b2p
+import B2plotter_flux as b2f
 import B2plotter_mid_calc as b2mc
 
 d = b2s.Setting_dic()
@@ -15,36 +16,44 @@ lex = b2s.loadDS_dic(d['DEV'])
 
 
 
-xl = b2p.Opacity_study(DEV = d['DEV'], withshift= d['withshift'], withseries= d['withseries'],
+xl = b2f.flux_adjustment(DEV = d['DEV'], withshift= d['withshift'], withseries= d['withseries'],
             DefaultSettings = d['DefaultSettings'], loadDS = lex, 
             Parameters= d['Parameters'], Publish= d['Publish'])
+xl.set_plot()
 xl.load_mast_dir()
 xl.load_solpsgeo()
-xl.calc_RRsep()
-xl.fitmastexp()
+xl.calc_RRsep(plotRR= False)
+xl.fitmastexp(writefile= False)
+xl.transport_coe_align_plot()
+xl.load_vessel()
+# xl.flux_comparison_plot()
+
 # xl.calcpsi()
-# xl.calc_RRsep()
-PL = '27'
-xl.calcpsi_1D(pol_loc= PL)
-# xl.calc_flux_expansion(polpos= '55')
-# xl.load_vessel()
-xl.set_plot()
-# xl.Opacity_study_radial_plot_dsa(pol_loc= '57')
-xl.Opacity_study_radial_plot(pol_loc= PL, x_choice= 'psiN')
 
-
-# poloidal_index_list = []
-# for i in range(38):
-#     poloidal_index_list.append('{}'.format(27 + i))
-# xl.calc_pol_angle(pol_list = poloidal_index_list)
-# xl.Opacity_study_poloidal_plot(pol_list= poloidal_index_list, x_choice= 'psiN')
+plot_flag = 'radial'
+if plot_flag == 'radial':
+    # xl.set_plot()
+    PL = '59'
+    xl.calcpsi_1D(pol_loc= PL)
+    xl.calc_dsa(pol_loc= PL)
+    xl.Opacity_study_radial_plot(pol_loc= PL, x_choice= 'psiN')
+elif plot_flag == 'poloidal':
+    # xl.set_plot()
+    poloidal_index_list = []
+    for i in range(47):
+        poloidal_index_list.append('{}'.format(25 + i))
+    xl.calc_pol_angle(pol_list = poloidal_index_list)
+    xl.Opacity_study_poloidal_plot(pol_list= poloidal_index_list, x_choice= 'psiN')
+elif plot_flag == 'skip':
+    pass
+else:
+    print('check plot_flag')
 
 
 # p = xl.data['dircomp']['Attempt']
 # q = xl.data['gfile']['gcomp']['check']
 # print(p)
 # print(q)
-
 
 
 
