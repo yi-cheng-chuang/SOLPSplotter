ec7e48109d99bb5baf5326d5d242934ea5973cc2
Yi-Cheng Chuang <ychuang@wm.edu>
2024-01-12 19:21:22 -0500

    Update SOLPSplotter_transcoe_adj.py

--- SOLPSplotter_transcoe_adj.py
+++ SOLPSplotter_transcoe_adj.py
@@ -4,35 +4,28 @@ Created on Thu Jan 11 15:00:38 2024
 
 @author: user
 """
-from B2plotter_class import B2plotter
+from SOLPSplotter_geo import Directory_and_Geometry
 import SOLPSplotter_set as sps
-import re
 import transport_coefficient_adjust_method as tcam
 import matplotlib.pyplot as plt
-import load_mast_expdata_method as lmem
-import load_coord_method as lcm
-import fitting_method as fm 
-from scipy import interpolate
-from scipy.interpolate import interp1d
-from scipy.optimize import curve_fit
 import numpy as np
-import xarray as xr
-import math
 
 
 
 
-class transport_coefficient_adjustment(B2plotter):
+class transport_coefficient_adjustment(Directory_and_Geometry):
     
-    def __init__(self, DEV, withshift, withseries, DefaultSettings, loadDS):
-        B2plotter.__init__(self, DEV, withshift, withseries, DefaultSettings)
-        # Employee.__init__(self, first, last, pay)
-        self.loadDS = loadDS
+    def __init__(self, DEV, withshift, withseries, DefaultSettings):
+        Directory_and_Geometry.__init__(self, DEV, withshift, 
+                                        withseries, DefaultSettings)
     
        
-    def mod_transco(self, withmod, de_SOL = 26, ki_SOL = 31, ke_SOL = 25):
+    def mod_transco(self, withmod, de_SOL, ki_SOL, ke_SOL, log_flag):
         
-        simu_dir = self.data['dirdata']['infolderdir']['org']['simudir']
+        
+        
+        
+        simu_dir = self.data['dirdata']['simudir']
         file_loc = '{}/b2.transport.inputfile_new'.format(simu_dir)
 
         trans_list = tcam.load_transcoefile_method(file_loc, plot= False)
@@ -73,35 +66,28 @@ class transport_coefficient_adjustment(B2plotter):
             pass
 
 
-        b = tcam.Generate_transcoefile_method(cod, CoeffID=1, SpeciesID=2, M=[1])
+        tcam.Generate_transcoefile_method(cod, CoeffID=1, SpeciesID=2, M=[1])
         
         shift = 'org'
-        n = self.data['dircomp']['Attempt']['org']
+        n = self.data['dircomp']['Attempt']
         
-        c = tcam.Write_transcoefile_method(file = 'b2.transport.inputfile_mod_{}{}'.format(shift, n), points= trans_list ,M_1 = True, M=[1])
+        tcam.Write_transcoefile_method(file = 'b2.transport.inputfile_mod_{}{}'.format(shift, n), points= trans_list ,M_1 = True, M=[1])
 
         log_flag = False
         specieslist = ['1','3','4']
-        d = 
-        i = 0
+        transcoe_unit = sps.transport_coe_unit()
 
         for k in specieslist:
             if log_flag:
                 plt.yscale('log')
-                plt.figure(i + 1)
-                plt.plot(trans_list[k][0,:], trans_list[k][1,:], 'o-', color = 'orange', label ='{} transport coefficient'.format(filename))
-                plt.xlabel('Radial coordinate: $R- R_{sep}$')
-                plt.ylabel(d[k][1])
-                plt.title(d[k][0])
-                plt.legend()
             else:
-                plt.figure(i + 1)
-                plt.plot(trans_list[k][0,:], trans_list[k][1,:], 'o-', color = 'orange', label ='{} transport coefficient'.format(filename))
-                plt.xlabel('Radial coordinate: $R- R_{sep}$')
-                plt.ylabel(d[k][1])
-                plt.title(d[k][0])
-                plt.legend()
-            i = i + 1
+                pass
+            plt.figure(figsize=(7,7))
+            plt.plot(trans_list[k][0,:], trans_list[k][1,:], 'o-', color = 'orange')
+            plt.xlabel('Radial coordinate: $R- R_{sep}$')
+            plt.ylabel(transcoe_unit[k][1])
+            plt.title(transcoe_unit[k][0])
+            plt.legend()
 
         plt.show()
     
