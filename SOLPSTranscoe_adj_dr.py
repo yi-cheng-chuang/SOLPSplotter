6c88b7d9635e45ee42c2e235b674e5f2f7d12052
Yi-Cheng Chuang <ychuang@wm.edu>
2024-01-24 16:21:12 -0500

    Update SOLPSTranscoe_adj_dr.py

--- SOLPSTranscoe_adj_dr.py
+++ SOLPSTranscoe_adj_dr.py
@@ -19,7 +19,7 @@ xt.load_solpsgeo()
 xt.calcpsi()
 xt.mod_transco(withmod = False, de_SOL = 24, ki_SOL = 31, ke_SOL = 23, log_flag = False)
 xt.transport_coe_align_plot(plot_transcoe = True)
-xt.align_transco(plot_align = True)
+xt.align_transco(plot_align = False)
 
 basedrt, topdrt, tpdrt = sps.set_wdir()
 
