6a111f6fe6a65fed3c4a8467132a01a137cad9a0
Yi-Cheng Chuang <ychuang@wm.edu>
2024-01-30 21:38:58 -0500

    Update SOLPSplotter_dr.py

--- SOLPSplotter_dr.py
+++ SOLPSplotter_dr.py
@@ -27,6 +27,7 @@ fitmastexp_setting_dic = {'writefile': True, 'plot_solps_fit': False,
 xl.fitmastexp(plot_setting_dic = fitmastexp_setting_dic)
 xl.load_b2fstate()
 xl.load_b2fplasmf()
+xl.b2fplasmf_filter()
 # xl.load_vessel()
 # xl.flux_comparison_plot()
 
