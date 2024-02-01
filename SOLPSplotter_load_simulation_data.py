ddaa6f646329f239af95eaaa8f37ac1f3c1e6154
yctok <ychuang@wm.edu>
2024-02-01 13:46:48 -0500

    Update SOLPSplotter_load_simulation_data.py

--- SOLPSplotter_load_simulation_data.py
+++ SOLPSplotter_load_simulation_data.py
@@ -8,6 +8,7 @@ Created on Tue Jan 30 14:11:08 2024
 
 from SOLPSplotter_load_expdata import load_expdata
 import load_B2_data_method as lbdm
+import load_Eirene_data_method as lEdm
 import numpy as np
 
 
@@ -194,6 +195,22 @@ class load_simu_data(load_expdata):
             self.data['DefaultSettings']['dims'] = dim_dic
             # self.b2fstate = state
         
+        elif self.withshift == False and self.withseries == True:
+            state_dic = {}
+            dim_dic = {}
+            
+            for aa in list(self.data['dircomp']['Attempt'].keys()):
+                file_loc = '{}/{}'.format(self.data['dirdata']['simudir'][aa], 'b2fstate')
+                state, dim = lbdm.read_b2fstate(b2fstateLoc = file_loc)
+                state_dic[aa] = vars(state)
+                dim_dic[aa] = {'nx': dim[0], 'ny': dim[1], 'ns': dim[2]}
+
+            self.data['b2fstate'] = state_dic
+            self.data['DefaultSettings']['dims'] = dim_dic
+            # self.b2fstate = state
+        
+        
+        
         else:
             print('load_b2fstate function is not there yet!')
     
@@ -230,135 +247,85 @@ class load_simu_data(load_expdata):
                 
                 
             self.data['b2fplasmf'] = fplasma_dic
+        
+        elif self.withshift == False and self.withseries == True:
+            fplasma_dic = {}
             
-        
-        
+            for aa in list(self.data['dircomp']['Attempt'].keys()):
+                file_loc = '{}/{}'.format(self.data['dirdata']['simudir'][aa], 'b2fplasmf')
+                dim_dic = self.data['DefaultSettings']['dims']
+                n_pol = dim_dic[aa]['nx']
+                n_rad = dim_dic[aa]['ny']
+                n_sp = dim_dic[aa]['ns']
+                fplasma = lbdm.read_b2fplasmf(fileName = file_loc, nx = n_pol, 
+                                              ny = n_rad, ns = n_sp)
+                fplasma_dic[aa] = vars(fplasma)
+                
+                
+            self.data['b2fplasmf'] = fplasma_dic
+            
+                
         else:
             print('load_b2fplasmf function is not there yet!')
-    
-    
+        
+        
+    """
+    there are two option of fort file to load, one is fort.44.i, the other
+    one is fort.46.i
+          
     """
     
-    This is the first version of filter, we can later isolate a 
-    general function from it so that can be used for b2fstate
-    
-    """
     
     
-    
-    
-    def b2fplasmf_filter_method(self, datafile, dim_setting):
-        nxny_list = []
-        zero_nxny_list = []
-               
-        nxnyns_list = []
-        zero_nxnyns_list = []
+    def load_ft46(self):
         
-        nxny_corner_list = []
-        zero_nxnycorner_list = []
+        ftname = 'fort.46.i'
         
-        fluxdim_ns_list = []
-        zero_fluxdimns_list = []
-        
-        nxny_corner_ns_list = []
-        zero_nxnycornerns_list = []
-        
-        rest_list = []
-        
-        nx = dim_setting['nx']
-        ny = dim_setting['ny']
-        ns = dim_setting['ns']
-        nc = 4
-               
-        # b2plasmf = datafile
-        for plasmf_k in list(datafile.keys()):
-            if np.shape(datafile[plasmf_k]) == (nx+2, ny+2):
-                if np.all(datafile[plasmf_k] == 0):
-                    zero_nxny_list.append(plasmf_k)
-                
-                else:
-                    nxny_list.append(plasmf_k)
-            
-            elif np.shape(datafile[plasmf_k]) == (nx+2, ny+2, ns):
-                for ns_a in range(ns):
-                    if np.all(datafile[plasmf_k][:, :, ns_a] == 0):
-                        zero_nxnyns_list.append('{}_{}'.format(plasmf_k, ns_a))
-                    
-                    else:
-                        nxnyns_list.append('{}_{}'.format(plasmf_k, ns_a))
-                    
-                
-            
-            elif np.shape(datafile[plasmf_k]) == (nx+2, ny+2, nc):
-                
-                for nc_a in range(4):
-                    if np.all(datafile[plasmf_k][:, :, nc_a] == 0):
-                        zero_nxnycorner_list.append('{}_{}'.format(plasmf_k, nc_a))
-                    
-                    else:
-                        nxny_corner_list.append('{}_{}'.format(plasmf_k, nc_a))
-            
-            
-            
-            elif np.shape(datafile[plasmf_k]) == (nx+2, ny+2, 2, 2):
-                
-                for ns_b in range(ns):
-                    for nf_a in range(2):
-                        if np.all(datafile[plasmf_k][:, :, nf_a, ns_b] == 0):
-                            zero_fluxdimns_list.append('{}_{}_ns{}'.format(plasmf_k, 
-                                                                    nf_a, ns_b))
-                        
-                        else:
-                            fluxdim_ns_list.append('{}_{}_ns{}'.format(plasmf_k, 
-                                                                    nf_a, ns_b))
-                          
-            elif np.shape(datafile[plasmf_k]) == (nx+2, ny+2, nc, ns):
-                for ns_c in range(ns):
-                    for nc_b in range(nc):
-                        if np.all(datafile[plasmf_k][:, :, nc_b, ns_c] == 0):
-                            zero_nxnycornerns_list.append('{}_nc{}_ns{}'.format(plasmf_k, 
-                                                                    nc_b, ns_c))
-                        
-                        else:
-                            nxny_corner_ns_list.append('{}_nc{}_ns{}'.format(plasmf_k, 
-                                                                    nc_b, ns_c))
-                           
-            
-            else:
-                rest_list.append(plasmf_k)
-                
-                    
-        
-        key_order_dic = {'nxny': nxny_list, 'zero_nxny': zero_nxny_list, 
-                         'nxnyns': nxnyns_list, 'zero_nxnyns': zero_nxnyns_list,
-                         'nxnycorner': nxny_corner_list, 'zero_nxnycorner': zero_nxnycorner_list,
-                         'fluxdim_ns': fluxdim_ns_list, 'zero_fluxdimns': zero_fluxdimns_list, 
-                         'nxny_corner_ns': nxny_corner_ns_list, 
-                         'zero_nxnycornerns': zero_nxnycornerns_list, 'the_rest': rest_list}
-        
-        
-        return key_order_dic
-    
-                    
-    
-    
-                    
-    def b2fplasmf_filter(self):
         if self.withshift == False and self.withseries == False:
-            print('single case: b2fplasmf_filter function is not there yet!')
+            file_loc = '{}/{}'.format(self.data['dirdata']['simudir'], '{}'.format(ftname))
+            ft46 = lEdm.read_ft46(fileName = file_loc)
+            ft46_dic = vars(ft46)
             
-        if self.withshift == True and self.withseries == False:
-            key_order_dic = {}
+            self.data['ft46'] = ft46_dic
+            # print('the next line is b2fplasmf')
+            # print(type(k))
+        
+        elif self.withshift == True and self.withseries == False:
+            ft46_dic = {}
             
             for aa in self.data['dircomp']['multi_shift']:
-                                
-                b2plasmf = self.data['b2fplasmf'][aa]
-                dim = self.data['DefaultSettings']['dims'][aa]
-                key_order_dic[aa] = self.b2fplasmf_filter_method(datafile = b2plasmf, 
-                                                                 dim_setting = dim)
+                
+                file_loc = '{}/{}'.format(self.data['dirdata']['simudir'][aa], '{}'.format(ftname))
+                ft46 = lEdm.read_ft46(fileName = file_loc)
+                ft46_dic[aa] = vars(ft46)
                 
                 
-            self.data['b2fplasmf_key'] = key_order_dic
+            self.data['ft46'] = ft46_dic
+        
+        elif self.withshift == False and self.withseries == True:
+            ft46_dic = {}
+            
+            for aa in list(self.data['dircomp']['Attempt'].keys()):
+                
+                file_loc = '{}/{}'.format(self.data['dirdata']['simudir'][aa], '{}'.format(ftname))
+                ft46 = lEdm.read_ft46(fileName = file_loc)
+                ft46_dic[aa] = vars(ft46)
+                
+                
+            self.data['ft46'] = ft46_dic
+            
+                
+        else:
+            print('load_b2fplasmf function is not there yet!')
+            
+            
+            
+
+            
+            
+    
+    
+    
                 
                 
             
