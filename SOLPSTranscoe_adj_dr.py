77b66f8869aa1bb0fa88f05f8d5a63446a6ff2c1
yctok <ychuang@wm.edu>
2024-02-08 14:19:28 -0500

    Update SOLPSTranscoe_adj_dr.py

--- SOLPSTranscoe_adj_dr.py
+++ SOLPSTranscoe_adj_dr.py
@@ -12,14 +12,13 @@ import SOLPS_transcoe_adj as sta
 
 d = sps.Setting_dic()
 
-xt = sta.transport_coefficient_adjustment(DEV = d['DEV'], withshift= d['withshift'], 
-        withseries= d['withseries'], DefaultSettings = d['DefaultSettings'])
+xt = sta.transport_coefficient_adjustment(DefaultSettings = d)
 xt.load_mast_dir()
 xt.load_solpsgeo()
 xt.calcpsi()
 xt.mod_transco(withmod = False, de_SOL = 24, ki_SOL = 31, ke_SOL = 23, log_flag = False)
 xt.transport_coe_align_plot(plot_transcoe = True)
-xt.align_transco(plot_align = False)
+xt.align_transco(plot_align = True)
 
 basedrt, topdrt, tpdrt = sps.set_wdir()
 
