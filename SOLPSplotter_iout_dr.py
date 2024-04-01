f38d2cd035d85bbc1bab229171bf44ee1a9bce43
Yi-Cheng Chuang <ychuang@wm.edu>
2024-03-31 23:14:23 -0400

    Update SOLPSplotter_iout_dr.py

--- SOLPSplotter_iout_dr.py
+++ SOLPSplotter_iout_dr.py
@@ -21,7 +21,7 @@ xl.load_mast_dir()
 xl.load_solpsgeo()
 # xl.calcpsi()
 xl.calcpsi_avcr()
-# xl.calc_RRsep(plotRR= False, plot_psi_dsa_align= False)
+xl.calc_RRsep(plotRR= False, plot_psi_dsa_align= False)
 fitmastexp_setting_dic = {'writefile': True, 'plot_solps_fit': False, 
                           'plot_exp_and_fit': False, 'plot_shift_compare': False,
                           'data_print': True}
@@ -29,8 +29,33 @@ xl.fitmastexp(plot_setting_dic = fitmastexp_setting_dic)
 xl.load_vessel()
 xl.load_ft44()
 xl.set_plot()
+xl.load_b2fstate()
 
-topic = 'Q3-1'
+xl.calc_sep_dsa()
+poloidal_index_list = []
+for i in range(44):
+    poloidal_index_list.append('{}'.format(25 + i))
+    
+xl.opacity_data_fit(pol_list = poloidal_index_list)
+xl.calc_pol_angle(pol_list = poloidal_index_list, plot_angle= False)
+
+# xl.opacity_poloidal_plot(log_flag = False, save_pdf = False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+topic = 'Q3'
 
 
 """
