9e572dc81d92e756f50d9fdef12988bc8e9c9daf
Yi-Cheng Chuang <ychuang@wm.edu>
2024-08-29 16:51:52 -0400

    Update SOLPSplotter_converge_dr.py

--- SOLPSplotter_converge_dr.py
+++ SOLPSplotter_converge_dr.py
@@ -6,14 +6,14 @@ Created on Thu Aug 29 14:22:49 2024
 """
 
 import SOLPS_set as sps
-import SOLPSplotter_NTplot as spn
+import SOLPSplotter_converge as spc
 
 d = sps.Setting_dic()
 lex = sps.loadDS_dic(d['DEV'])
 
 
 
-xl = spn.NT_plot(DefaultSettings = d, loadDS = lex)
+xl = spc.SOLPS_converge(DefaultSettings = d, loadDS = lex)
 
 xl.load_mast_dir()
 xl.load_solpsgeo()
@@ -31,7 +31,10 @@ xl.set_plot()
 poloidal_index_list = ['59']
 xl.calc_dsa(pol_loc= poloidal_index_list[0])
 
-
-
 xl.opacity_data_fit(pol_list = poloidal_index_list, dat_size = 'small', check_ne = False)
 xl.radial_data_fit(pol_loc = poloidal_index_list[0], dat_size = 'small', check_ne = False)
+
+xl.profile_combine(p_check = True)
+xl.load_numerical_midplane(data_size = 'small', plot = False)
+xl.solution_compare()
+
