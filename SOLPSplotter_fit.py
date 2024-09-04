11faf1d04d163c5394fd32af8dd6081a4f8b1adf
Yi-Cheng Chuang <ychuang@wm.edu>
2024-09-03 20:03:03 -0400

    Update SOLPSplotter_fit.py

--- SOLPSplotter_fit.py
+++ SOLPSplotter_fit.py
@@ -106,6 +106,7 @@ class profile_fit(RP_mapping):
         return result
     
     
+    
     def opacity_data_fit(self, pol_list, dat_size, check_ne):
         
         # self.load_ft44()
@@ -212,8 +213,8 @@ class profile_fit(RP_mapping):
                 pd = self.data['DefaultSettings']['psi_dsa']
                 
                 fitresult = self.opacity_data_fit_method(b2fstate = fstate, Neuden = Neuden_data,
-                            psiN = psiN_map, psi_dsa_ratio = pd, pol_list = pol_list,
-                                        itername = None, data_struc = dat_struc)
+                        psiN = psiN_map, psi_dsa_ratio = pd, pol_list = pol_list,
+                        itername = None, data_struc = dat_struc, check_ne = check_ne)
                 
                 fitresult_dic[aa] = fitresult
             
@@ -476,6 +477,7 @@ class profile_fit(RP_mapping):
         else:
             print('radial_data_fit has a bug')
     
+    
 
         
         
@@ -483,221 +485,6 @@ class profile_fit(RP_mapping):
 # ----------------------------------------------------------------------------           
     
     """
-    backup:
-        
-
-    
-    def opacity_data_method_single(self, pol_list): 
-        i = 0
-        ln = len(pol_list)
-        efold = np.zeros(ln)
-        efold_l = np.zeros(ln)
-        delta = np.zeros(ln)
-        delta_l = np.zeros(ln)
-        opq = np.zeros(ln)
-        # pol_loc = np.zeros(ln)
-        neu_den = np.zeros(ln)
-        ne_ped = np.zeros(ln)
-        tdelta = np.zeros(ln)
-        fluxexp = np.zeros(ln)
-        
-        
-        for k in pol_list:
-            psi = self.data['psi']['psi_{}_val'.format(k)][:, 2]
-            
-            # psi_RGI = self.data['psi']['psi_{}_val'.format(pol_loc)][:, 0]
-            # SEP = int(self.data['DefaultSettings']['SEP'])
-            
-            pol_in = int(k)
-            Nd = self.data['outputdata']['NeuDen'][:, pol_in]
-            Ne = self.data['outputdata']['Ne'][:, pol_in]
-            Te = self.data['outputdata']['Te'][:, pol_in]
-            
-
-            rd = fm.Opacity_calculator(x_coord= psi, ne = Ne, te = Te, 
-                                   neuden = Nd)
-            ped_index = rd['sep_index']
-            
-            fe = self.calc_flux_expansion(pol_loc= k, 
-                            ped_index= ped_index, iter_index= None)
-            pd = self.data['DefaultSettings']['psi_dsa']
-            
-
-            efold[i] = rd['efold_length']
-            delta[i] = rd['pedestal_width']
-            opq[i] = rd['dimensionless_opaqueness']
-            neu_den[i] = rd['n_sep_fit']
-            ne_ped[i] = rd['electron_pedestal_density']
-            tdelta[i] = rd['temperature_pedestal_width']
-            fluxexp[i] = fe
-            efold_l[i] = rd['efold_length']*pd*fe
-            delta_l[i] = rd['pedestal_width']*pd*fe
-            
-            
-            # pol_loc[i] = int(k)
-            i = i + 1
-        
-        result = {'efold_length_psiN': efold, 'pedestal_width_psiN': delta,
-                  'dimensionless_opaqueness': opq, 
-                  'neutral_density': neu_den, 
-                  'electron_pedestal_density': ne_ped,
-                  'temperature_pedestal_width': tdelta,
-                  'flux_expansion': fluxexp,
-                  'efold_length': efold_l, 'pedestal_width': delta_l,
-                                  
-                  }
-        
-        return result
-
-    def opacity_data_method_multi(self, pol_list, iter_list):
-        efold_dic = {}
-        efold_leng_dic = {}
-        delta_dic = {}
-        delta_leng_dic = {}
-        opq_dic = {}
-        neu_den_dic = {}
-        ne_ped_dic = {}
-        tdelta_dic = {}
-        flux_expand_dic = {}
-        xcoord_cut_dic = {}
-        
-        for aa in iter_list:
-            i = 0
-            ln = len(pol_list)
-            efold = np.zeros(ln)
-            efold_l = np.zeros(ln)
-            delta = np.zeros(ln)
-            delta_l = np.zeros(ln)
-            opq = np.zeros(ln)
-            neu_den = np.zeros(ln)
-            ne_ped = np.zeros(ln)
-            tdelta = np.zeros(ln)
-            flux_exp = np.zeros(ln)
-            xcoord_cut = {}
-        
-            
-            
-            for k in pol_list:
-                
-                if self.withshift == True and self.withseries == False:
-                    psi = self.data['psi']['psi_{}_val'.format(k)][aa][:, 2]
-                elif self.withshift == False and self.withseries == True:
-                    psi = self.data['psi']['psi_{}_val'.format(k)][:, 2]
-                else:
-                    print('out of expectation')
-                
-                
-                # psi_RGI = self.data['psi']['psi_{}_val'.format(pol_loc)][:, 0]
-                # SEP = int(self.data['DefaultSettings']['SEP'])
-                
-                pol_in = int(k)
-                Nd = self.data['outputdata']['NeuDen'][aa][:, pol_in]
-                Ne = self.data['outputdata']['Ne'][aa][:, pol_in]
-                Te = self.data['outputdata']['Te'][aa][:, pol_in]
-                
-                rd = fm.Opacity_calculator(x_coord = psi, ne = Ne, te = Te,
-                                           neuden = Nd)
-                
-                xcoord_cut_index = {k: rd['x_coord_cut']}
-                xcoord_cut = xcoord_cut | xcoord_cut_index
-                
-                ped_index = rd['sep_index']
-                
-                fe = self.calc_flux_expansion(pol_loc= k, 
-                                ped_index= ped_index, iter_index= aa)
-                
-                if self.withshift == True and self.withseries == False:
-                    pd = self.data['DefaultSettings']['psi_dsa'][aa]
-                elif self.withshift == False and self.withseries == True:
-                    pd = self.data['DefaultSettings']['psi_dsa']
-                else:
-                    print('there is a bug')
-
-                
-                
-                efold[i] = rd['efold_length']
-                efold_l[i] = rd['efold_length']*fe*pd
-                delta[i] = rd['pedestal_width']
-                delta_l[i] = rd['pedestal_width']*fe*pd
-                opq[i] = rd['dimensionless_opaqueness']
-                neu_den[i] = rd['n_sep_fit']
-                ne_ped[i] = rd['electron_pedestal_density']
-                tdelta[i] = rd['temperature_pedestal_width']
-                flux_exp[i] = fe
-
-                # pol_loc[i] = int(k)
-                i = i + 1
-            
-            efold_dic[aa] = efold
-            efold_leng_dic[aa] = efold_l
-            delta_dic[aa] = delta
-            delta_leng_dic[aa] = delta_l
-            opq_dic[aa] = opq
-            neu_den_dic[aa] = neu_den
-            ne_ped_dic[aa] = ne_ped
-            tdelta_dic[aa] = tdelta
-            flux_expand_dic[aa] = flux_exp
-            xcoord_cut_dic[aa] = xcoord_cut
-            
-            
-        
-        result = {'efold_length_psiN': efold_dic, 
-                  'pedestal_width_psiN': delta_dic,
-                  'efold_length': efold_leng_dic, 
-                  'pedestal_width': delta_leng_dic,
-                  'dimensionless_opaqueness': opq_dic, 
-                  'neutral_density': neu_den_dic, 
-                  'electron_pedestal_density': ne_ped_dic,
-                  'temperature_pedestal_width': tdelta_dic,
-                  'flux_expansion': flux_expand_dic               
-                  }
-        self.data['xcoord_cut'] = xcoord_cut_dic
-        
-        return result
-
-
-    def data_support(self, data_source):
-        
-        if data_source == 'output':
-            
-            self.load_output_data(param= 'NeuDen')
-            self.load_output_data(param= 'Ne')
-            self.load_output_data(param= 'Te')
-        
-        elif data_source == 'b2f':
-            
-            print('wait for improvement')
-            
-            
-
-            
-            if self.data['b2fstate'] == None:
-                self.load_b2fstate()
-                
-                b2fstate = self.data['b2fstate']
-                
-                Ne_data = b2fstate['ne'].transpose()
-                Te_J = b2fstate['te'].transpose()
-                ev = 1.6021766339999999 * pow(10, -19)
-                Te_data = Te_J / ev
-                
-            elif self.data['b2fstate'] != None:
-                
-                Ne_data = b2fstate['ne'].transpose()
-                Te_J = b2fstate['te'].transpose()
-                ev = 1.6021766339999999 * pow(10, -19)
-                Te_data = Te_J / ev
-                
-            if self.data['b2fplasmf'] == None:
-                self.load_b2fplasmf()
-                Neuden_data = 
-                
-                
-            elif self.data['b2fstate'] != None:
-                
-              
-
-
 
 
 
