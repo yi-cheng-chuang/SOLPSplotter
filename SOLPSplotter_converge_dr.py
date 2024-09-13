d89396a3cb3fe19cd0c26f2e09ec9084803c8f94
Yi-Cheng Chuang <ychuang@wm.edu>
2024-09-13 11:10:11 -0400

    Update SOLPSplotter_converge_dr.py

--- SOLPSplotter_converge_dr.py
+++ SOLPSplotter_converge_dr.py
@@ -36,6 +36,6 @@ xl.radial_data_fit(pol_loc = poloidal_index_list[0], dat_size = 'small', check_n
 
 xl.profile_combine(p_check = True)
 xl.load_numerical_midplane(data_size = 'small', plot = False)
-xl.solution_compare(plot = False)
+xl.solution_compare(plot = True)
 xl.error_calculation()
 
