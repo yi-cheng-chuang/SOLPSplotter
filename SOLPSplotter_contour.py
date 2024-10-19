7bc2581d0ae4df1b987d60d390a2c7887cd57ecc
Yi-Cheng Chuang <ychuang@wm.edu>
2024-10-19 13:09:51 -0400

    Update SOLPSplotter_contour.py

--- SOLPSplotter_contour.py
+++ SOLPSplotter_contour.py
@@ -71,7 +71,7 @@ class PlotContour(profile_fit):
         
         vessel = self.data['vessel'][itername]
         
-        fig, axs = plt.subplots()
+        
         if log_bar:
             if np.all(plot_2dval == 0):
                 print('data_file is an zero matrix')
@@ -89,6 +89,8 @@ class PlotContour(profile_fit):
                 
                 smap = cm.ScalarMappable(Lnorm, CPB)    
                 plt.colorbar(smap)
+                
+                
             else:
                 
                 datamap = np.abs(plot_2dval)
@@ -147,7 +149,304 @@ class PlotContour(profile_fit):
             
         plt.plot(vessel[:,0]/1000, vessel[:,1]/1000, color = color_dic[itername])
                 
+    
+    
+    
+    def improve_paper_plot(self, plotstyle):
         
+        if self.withshift == True and self.withseries == False:
+            
+            color_dic = {'org': 'red', 'dot3': 'orange', 'dot5': 'green',
+                         'dot7': 'blue', 'one': 'purple'}
+            
+            A_dic = {'org': '1.4', 'dot3': '2.0', 'dot5': '2.4',
+                      'dot7': '2.8', 'one': '3.4'}
+            
+            if plotstyle == 'single':
+                
+                for aa in self.data['dircomp']['multi_shift']:
+                    
+                    
+                    neuden = self.data['ft44'][aa]['dab2'][:, :, 0]
+                    Radloc = np.transpose(self.data['grid']['RadLoc'][aa])[1:97, 1:37]
+                    VertLoc = np.transpose(self.data['grid']['VertLoc'][aa])[1:97, 1:37]
+                    
+                    
+                    self.contour_plot(plot_2dval = neuden, R_coord = Radloc , 
+                    Z_coord = VertLoc, quantity = 'Neutral density', itername = aa, 
+                             log_bar = True, ma100 = False, bounds = [], 
+                             color_dic = color_dic, A_dic = A_dic)
+            
+            
+            elif plotstyle == 'paper':
+                
+                
+                comp_list = ['org', 'dot7']
+                
+                fig, axs = plt.subplots(1, 2, sharey= True)
+                
+                org_text = AnchoredText('{}'.format('(a) A = 1.4'), 
+                                             loc='upper center')
+                
+                dot7_text = AnchoredText('{}'.format('(b) A = 2.8'), 
+                                             loc='upper center')
+                
+                
+                text_list = [org_text, dot7_text]
+                
+                
+
+                for ii, aa in enumerate(comp_list):
+                    
+                    neuden = self.data['ft44'][aa]['dab2'][:, :, 0]
+                    RadLoc = np.transpose(self.data['grid']['RadLoc'][aa])[1:97, 1:37]
+                    VertLoc = np.transpose(self.data['grid']['VertLoc'][aa])[1:97, 1:37]
+                    
+                    
+                    if np.all(neuden == 0):
+                        print('data_file is an zero matrix')
+                        
+                    elif np.any(neuden == 0):
+                        plot_2dval = ma.masked_where(neuden <= 0, neuden)
+                        
+                        datamap = np.abs(plot_2dval)
+                    
+                    else:
+                        
+                        datamap = neuden
+                    
+                    
+                    CPB = cm.viridis
+                    Lnorm = LogNorm(vmax = datamap.max(), vmin = datamap.min())
+                    
+                    
+                    self.paper_contour(plot_2dval = neuden, R_coord = RadLoc, Z_coord = VertLoc, 
+                            quantity = 'Neutral density', itername = aa, 
+                log_bar = True, color_dic = color_dic, A_dic = A_dic, axs = axs[ii], 
+                cmap = CPB, norm = Lnorm, levels = 20)
+                    
+                    axs[ii].add_artist(text_list[ii])
+                    axs[ii].set_xlabel('R [m]')
+                    
+                
+                fig.supylabel('Z [m]')
+                # fig.supxlabel('R [m]')
+                fig.suptitle('Atomic neutral density $[m^{-3}]$ contour plot')
+                smap = cm.ScalarMappable(Lnorm, CPB)    
+                fig.colorbar(smap)
+                plt.tight_layout()
+    
+    
+    
+    def rebuttal_NTplot(self, plotstyle):
+        
+        if self.withshift == True and self.withseries == False:
+            
+            color_dic = {'org': 'red', 'dot3': 'orange', 'dot5': 'green',
+                          'dot7': 'blue', 'one': 'purple'}
+            
+            A_dic = {'org': '1.4', 'dot3': '2.0', 'dot5': '2.4',
+                      'dot7': '2.8', 'one': '3.4'}
+            
+
+            if plotstyle == 'paper':
+                
+                
+                comp_list = ['org', 'dot3', 'dot5', 'dot7']
+                
+                fig, axs = plt.subplots(1, 4, sharey= True)
+                
+                org_text = AnchoredText('{}'.format('(a) A = 1.4'), 
+                                              loc='upper center')
+                
+                dot3_text = AnchoredText('{}'.format('(b) A = 2.0'), 
+                                              loc='upper center')
+                
+                dot5_text = AnchoredText('{}'.format('(c) A = 2.4'), 
+                                              loc='upper center')
+                
+                dot7_text = AnchoredText('{}'.format('(d) A = 2.8'), 
+                                              loc='upper center')
+                
+                
+                text_list = [org_text, dot3_text, dot5_text, dot7_text]
+                
+                
+
+                for ii, aa in enumerate(comp_list):
+                    
+                    
+                    
+                    
+                    Te_J = self.data['b2fstate'][aa]['te'][1:97, 1:37]
+                    
+                    ev = 1.6021766339999999 * pow(10, -19)
+                    neuden = Te_J / ev
+                    
+                    
+                    RadLoc = np.transpose(self.data['grid']['RadLoc'][aa])[1:97, 1:37]
+                    VertLoc = np.transpose(self.data['grid']['VertLoc'][aa])[1:97, 1:37]
+                    
+                    
+                    if np.all(neuden == 0):
+                        print('data_file is an zero matrix')
+                        
+                    elif np.any(neuden == 0):
+                        plot_2dval = ma.masked_where(neuden <= 0, neuden)
+                        
+                        datamap = np.abs(plot_2dval)
+                    
+                    else:
+                        
+                        datamap = neuden
+                    
+                    
+                    CPB = cm.viridis
+                    Lnorm = LogNorm(vmax = datamap.max(), vmin = datamap.min())
+                    
+                    
+                    self.paper_contour(plot_2dval = neuden, R_coord = RadLoc, Z_coord = VertLoc, 
+                            quantity = 'Electron density', itername = aa, 
+                log_bar = True, color_dic = color_dic, A_dic = A_dic, axs = axs[ii], 
+                cmap = CPB, norm = Lnorm, levels = 20)
+                    
+                    axs[ii].add_artist(text_list[ii])
+                    axs[ii].set_xlabel('R [m]')
+                    
+                
+                axs[0].set_ylabel('Z [m]')
+                fig.suptitle('Electron temperature $[eV]$ contour plot')
+                smap = cm.ScalarMappable(Lnorm, CPB)    
+                fig.colorbar(smap)
+                plt.tight_layout(w_pad = 0.05)
+    
+    
+    
+    
+    def rebuttal_NTchpplot(self, plotstyle):
+        
+        if self.withshift == True and self.withseries == False:
+            
+            color_dic = {'org': 'red', 'dot3': 'orange', 'dot5': 'green',
+                          'dot7': 'blue', 'one': 'purple'}
+            
+            A_dic = {'org': '1.4', 'dot3': '2.0', 'dot5': '2.4',
+                      'dot7': '2.8', 'one': '3.4'}
+            
+
+            if plotstyle == 'paper':
+                
+                
+                comp_list = ['org', 'dot3', 'dot5', 'dot7']
+                
+                fig, axs = plt.subplots(1, 4, sharey= True)
+                
+                org_text = AnchoredText('{}'.format('(a) A = 1.4'), 
+                                              loc='upper center')
+                
+                dot3_text = AnchoredText('{}'.format('(b) A = 2.0'), 
+                                              loc='upper center')
+                
+                dot5_text = AnchoredText('{}'.format('(c) A = 2.4'), 
+                                              loc='upper center')
+                
+                dot7_text = AnchoredText('{}'.format('(d) A = 2.8'), 
+                                              loc='upper center')
+                
+                
+                text_list = [org_text, dot3_text, dot5_text, dot7_text]
+                
+                
+
+                for ii, aa in enumerate(comp_list):
+                    
+                    neuden = self.data['b2fstate'][aa]['na'][1:97, 1:37, 1]
+                    
+                    
+                    RadLoc = np.transpose(self.data['grid']['RadLoc'][aa])[1:97, 1:37]
+                    VertLoc = np.transpose(self.data['grid']['VertLoc'][aa])[1:97, 1:37]
+                    
+                    
+                    if np.all(neuden == 0):
+                        print('data_file is an zero matrix')
+                        
+                    elif np.any(neuden == 0):
+                        plot_2dval = ma.masked_where(neuden <= 0, neuden)
+                        
+                        datamap = np.abs(plot_2dval)
+                    
+                    else:
+                        
+                        datamap = neuden
+                    
+                    
+                    CPB = cm.viridis
+                    NORM= plt.Normalize(datamap.min(), datamap.max())
+                    
+                    
+                    self.paper_contour(plot_2dval = neuden, R_coord = RadLoc, Z_coord = VertLoc, 
+                            quantity = 'Electron density', itername = aa, 
+                log_bar = True, color_dic = color_dic, A_dic = A_dic, axs = axs[ii], 
+                cmap = CPB, norm = NORM, levels = 20)
+                    
+                    axs[ii].add_artist(text_list[ii])
+                    axs[ii].set_xlabel('R [m]')
+                    
+                
+                axs[0].set_ylabel('Z [m]')
+                fig.suptitle('Electron temperature $[eV]$ contour plot')
+                smap = cm.ScalarMappable(NORM, CPB)    
+                fig.colorbar(smap)
+                plt.tight_layout(w_pad = 0.05)
+    
+    
+    
+    def paper_contour(self, plot_2dval, R_coord, Z_coord, quantity, itername, 
+                     log_bar, color_dic, A_dic, axs, cmap, norm, levels):
+        
+        
+        vessel = self.data['vessel'][itername]
+        
+        
+        if log_bar:
+            if np.all(plot_2dval == 0):
+                print('data_file is an zero matrix')
+                
+            elif np.any(plot_2dval == 0):
+                plot_2dval = ma.masked_where(plot_2dval <= 0, plot_2dval)
+                
+                datamap = np.abs(plot_2dval)
+                
+                
+                # CPB = cm.viridis
+                # Lnorm = LogNorm(vmax = datamap.max(), vmin = datamap.min())
+                axs.contourf(R_coord, Z_coord, datamap, levels= levels, 
+                             cmap = cmap, norm = norm)
+                
+                
+            else:
+                
+                datamap = np.abs(plot_2dval)
+                
+                # CPB = cm.viridis
+                # Lnorm = LogNorm(vmax = datamap.max(), vmin = datamap.min())
+                
+                axs.contourf(R_coord, Z_coord, datamap,levels= levels, 
+                             cmap = cmap, norm = norm)
+                
+        else:
+            
+
+            # NORM = axs.Normalize(plot_2dval.min(), plot_2dval.max())
+            
+            # CMAP = cm.viridis
+            
+            axs.contourf(R_coord, Z_coord, plot_2dval, levels= levels, cmap= cmap, norm = norm)
+            
+            
+        axs.plot(vessel[:,0]/1000, vessel[:,1]/1000, color = 'black')
+        # axs.plot(vessel[:,0]/1000, vessel[:,1]/1000, color = color_dic[itername])
+                
         
     
     def flux_expansion_contour_plot_method(self, RR_sep, flux_expand_map, itername):
@@ -526,52 +825,7 @@ class PlotContour(profile_fit):
             
         
         
-    def paper_contour(self, plot_2dval, R_coord, Z_coord, quantity, itername, 
-                     log_bar, color_dic, A_dic, axs, cmap, norm, levels):
-        
-        
-        vessel = self.data['vessel'][itername]
-        
-        
-        if log_bar:
-            if np.all(plot_2dval == 0):
-                print('data_file is an zero matrix')
-                
-            elif np.any(plot_2dval == 0):
-                plot_2dval = ma.masked_where(plot_2dval <= 0, plot_2dval)
-                
-                datamap = np.abs(plot_2dval)
-                
-                
-                # CPB = cm.viridis
-                # Lnorm = LogNorm(vmax = datamap.max(), vmin = datamap.min())
-                axs.contourf(R_coord, Z_coord, datamap, levels= levels, 
-                             cmap = cmap, norm = norm)
-                
-                
-            else:
-                
-                datamap = np.abs(plot_2dval)
-                
-                # CPB = cm.viridis
-                # Lnorm = LogNorm(vmax = datamap.max(), vmin = datamap.min())
-                
-                axs.contourf(R_coord, Z_coord, datamap,levels= levels, 
-                             cmap = cmap, norm = norm)
-                
-                
-            
-        else:
-            
-
-            # NORM = axs.Normalize(plot_2dval.min(), plot_2dval.max())
-            
-            # CMAP = cm.viridis
-            
-            axs.contourf(R_coord, Z_coord, plot_2dval, levels= levels, cmap= cmap, norm = norm)
-            
-            
-        axs.plot(vessel[:,0]/1000, vessel[:,1]/1000, color = color_dic[itername])   
+       
     
     
 
