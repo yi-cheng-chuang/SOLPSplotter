71a0a64d06e6efe82b749ee8d980255d22cbeae8
Yi-Cheng Chuang <ychuang@wm.edu>
2025-03-25 07:42:23 -0400

    Update SOLPSplotter_ndscan_dr.py

--- SOLPSplotter_ndscan_dr.py
+++ SOLPSplotter_ndscan_dr.py
@@ -49,7 +49,7 @@ xl.load_fluxes_iout()
 # xl.multirad_data_fit(pol_list = pol_list, dat_size = 'small', check_ne = False)
 
 
-xl.twinscan_ndrad_plot(scan_style = 'tempscan', dat_size = 'small', log_flag = True, 
+xl.twinscan_ndrad_plot(scan_style = 'tempscan', dat_size = 'small', log_flag = False, 
                        format_option= '1x1', xcoord_type = 'rrsep')
 
 
