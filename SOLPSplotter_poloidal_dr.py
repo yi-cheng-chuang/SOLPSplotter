e0b5458154b6ba0cf291c48fa5a140a4f0b825e1
Yi-Cheng Chuang <ychuang@wm.edu>
2025-03-31 10:36:24 -0400

    Update SOLPSplotter_poloidal_dr.py

--- SOLPSplotter_poloidal_dr.py
+++ SOLPSplotter_poloidal_dr.py
@@ -25,7 +25,7 @@ fitmastexp_setting_dic = {'writefile': True, 'plot_solps_fit': False,
                           'data_print': True}
 xl.fitmastexp(plot_setting_dic = fitmastexp_setting_dic)
 xl.load_b2fstate()
-xl.load_b2fplasmf()
+# xl.load_b2fplasmf()
 # xl.load_output_data(param= 'Te')
 xl.calc_sep_dsa()
 xl.load_ft44()
