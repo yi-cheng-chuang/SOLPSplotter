c176b4d362c13b8af41a49ca7c8576da525f0085
Yi-Cheng Chuang <ychuang@wm.edu>
2025-04-13 22:28:21 -0400

    Create header.py

---
+++ SOLPS_input/header.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 13 20:51:52 2025
+
+@author: ychuang
+"""
+
+
+
+import matplotlib.pyplot as plt 
+import numpy as np
+from matplotlib.offsetbox import AnchoredText
+from scipy import interpolate
+
+
+
+
+__all__ = ['np', 'plt', 'AnchoredText', 'interpolate']
