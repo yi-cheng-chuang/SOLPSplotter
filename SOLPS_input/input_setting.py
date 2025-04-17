f0ef221e38ab486dd8aad6a04801efab0dbb7a5c
Yi-Cheng Chuang <ychuang@wm.edu>
2025-04-17 11:48:52 -0400

    Update input_setting.py

--- SOLPS_input/input_setting.py
+++ SOLPS_input/input_setting.py
@@ -24,8 +24,8 @@ def Setting_dic():
             
             self.DEV = 'mast'
             self.a = 0.5
-            self.withshift = True
-            self.withseries = False
+            self.withshift = False
+            self.withseries = True
             self.terminal = terminal
             if self.withseries:
                 self.series_flag = 'twin_scan'
@@ -156,34 +156,7 @@ OPTIONAL:
     ROOTSHOT = '' > Optional numerical prefix to designate experimental campaign that a series of shots may share in common\n 
 """
 
-P = {'Ne': r'Electron Density $n_e\;(m^{-3})$',
-        'Te': r'Electron Temperature $T_e\;(eV)$',
-        'NI' : r'Ion (D+) Density $n_i\;(m^{-3})$',
-        'Ti': r'Ion (D+) Temperature $T_i\;(eV)$',
-        'DN': r'Particle Density Diffusivity $D\;(m^2/s)$',
-        'KYE': r'Electron Thermal Diffusivity $\chi_e (m^2/s)$',
-        'KYI': r'Ion Thermal Diffusivity $\chi_i (m^2/s)$',
-        'NeuDen': r'Neutral Atom (D) Density $(m^{-3})$',
-        'MolDen': r'Neutral Molecule (D2) Density $(m^{-3})$',
-        'NeuTemp': r'Neutral Atom (D) Temperature (eV)',
-        'MolTemp': r'Neutral Molecule (D2) Temperature (eV)',
-        'IonFlx': r'Radial Ion (D+) Flux $s^{-1}$',
-        'IonPol': r'Poloidal Ion (D+) Flux $s^{-1}$',
-        'MolFlx': r'Radial Molecule Flux $m^{-2}s^{-1}$',
-        'RadFlx': r'Radial Atomic Flux $m^{-2}s^{-1}$',
-        'VLY': r'Radial Pinch $v_y (m^2/s)$',
-        'SX': r'Poloidal Contact Area SX $(m^{2})$',
-        'SY': r'Radial Contact Area SY $(m^{2})$',
-        'SZ': r'Poloidal Cross-Sectional Area SZ $(m^{2})$',
-        'VOL': r'Cell Volume VOL $(m^{3})$',
-        'RadPinch': r'Anomalous Radial Pinch Velocity $v_y\;(m/s)$',
-        'AHAL': r'Atomic $H_\alpha$ emissivity $(photons*m^{-3}s^{-1})$',
-        'MHAL': r'Molecular $H_\alpha$ emissivity $(photons*m^{-3}s^{-1})$',
-        'NTI' : r'Test Ion (D2+) Density $n_{ti}\;(m^{-3})$',
-        'TestIonTemp' : r'Test Ion (D2+) Temperature $T_{testion}\;(eV)$',
-        'LyaEmiss' : r'Lyman-alpha Emissivity $(photons*m^{-3}s^{-1}sr^{-1})$',
-        'LyaEmissW' : r'Converted Lyman-alpha Emissivity $(W*m^{-3})$',
-        'LyaEmissW_IE' : r'Ion-Electron Reaction Lyman-alpha Emissivity $(W*m^{-3})$'}
+
 
 DP_backup = {'LOG10' : 0,
         'GRAD' : False,
