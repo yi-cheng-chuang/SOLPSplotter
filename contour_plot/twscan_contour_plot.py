36fb1df2b15f671a89e4eb12dfa6d6c590f42b05
Yi-Cheng Chuang <ychuang@wm.edu>
2025-04-18 00:29:27 -0400

    Update twscan_contour_plot.py

--- contour_plot/twscan_contour_plot.py
+++ contour_plot/twscan_contour_plot.py
@@ -6,8 +6,6 @@ Created on Tue Apr  1 21:10:06 2025
 """
 
 
-
-
 from matplotlib.offsetbox import AnchoredText
 import numpy as np
 import matplotlib.pyplot as plt
@@ -48,7 +46,7 @@ class twscan_contour:
     
     
     
-    def twscan_contour_plot(self, dat_size, scan_style):
+    def twscan_contour_plot(self, scan_style):
         
 
         nx = self.data['b2fgeo']['nx']
@@ -58,73 +56,76 @@ class twscan_contour:
         RadLoc = np.transpose(self.data['grid']['RadLoc'])[1:nx + 1, 1:ny + 1]
         VertLoc = np.transpose(self.data['grid']['VertLoc'])[1:nx + 1, 1:ny + 1]
             
-            
+        
+        withshift = self.DF.withshift
+        withseries = self.DF.withseries
         
         
-        if self.series_flag == 'twin_scan':
+        
+        if withshift == False and withseries == True:
             
-            dircomp = self.data['dircomp']
             
-            if scan_style == 'tempscan':
+            
+            if self.DF.series_flag == 'twin_scan':
                 
-                key_a = 'denscan_list'
-                key_b = 'tempscan_list'
-            
-            elif scan_style == 'denscan':
+                dircomp = self.data['dircomp']
                 
-                key_a = 'tempscan_list'
-                key_b = 'denscan_list'
-            
-            else:
-                print('twinscan_plot_method, please check the scan_style!')
-            
-            keylist_a = []
-            
-            
-            for x in dircomp[key_a]:
-                keylist_a.append('{:.3f}'.format(x))
-            
-            for ta in keylist_a:
+                if scan_style == 'tempscan':
+                    
+                    key_a = 'denscan_list'
+                    key_b = 'tempscan_list'
                 
-                keylist_b = []
+                elif scan_style == 'denscan':
+                    
+                    key_a = 'tempscan_list'
+                    key_b = 'denscan_list'
                 
-                for x in dircomp[key_b]:
-                    keylist_b.append('{:.3f}'.format(x))
+                else:
+                    print('twinscan_plot_method, please check the scan_style!')
                 
-                # color_list = ['red', 'orange', 'green', 'blue', 'purple']
+                keylist_a = []
                 
-                # color_dic = self.pair_dic(keys = keylist_b, values = color_list)
                 
+                for x in dircomp[key_a]:
+                    keylist_a.append('{:.3f}'.format(x))
+                
+                for ta in keylist_a:
+                    
+                    keylist_b = []
+                    
+                    for x in dircomp[key_b]:
+                        keylist_b.append('{:.3f}'.format(x))
+                    
+                    # color_list = ['red', 'orange', 'green', 'blue', 'purple']
+                    
+                    # color_dic = self.pair_dic(keys = keylist_b, values = color_list)
+                    
 
-                # print('scan_list after initial:')
-                # print(scan_list)
-                iterlist = []
-                
-                
-                for tb in keylist_b:
+                    # print('scan_list after initial:')
+                    # print(scan_list)
+                    iterlist = []
                     
-                    if scan_style == 'tempscan':
+                    
+                    for tb in keylist_b:
                         
-                        it_in = (ta, tb)
-                    
-                    elif scan_style == 'denscan':
+                        if scan_style == 'tempscan':
+                            
+                            it_in = (ta, tb)
                         
-                        it_in = (tb, ta)
-                    
-                    else:
-                        print('twinscan_plot_method, please check the scan_style!')
-                    
-                    iterlist.append(it_in)
-                
-                
-                
-                for aa in iterlist:
-                    
-                    
-                    
-                    
-                    if self.series_flag == 'twin_scan':
+                        elif scan_style == 'denscan':
+                            
+                            it_in = (tb, ta)
                         
+                        else:
+                            print('twinscan_plot_method, please check the scan_style!')
+                        
+                        iterlist.append(it_in)
+                    
+                    
+                    
+                    for aa in iterlist:
+                        
+                            
                         if scan_style == 'tempscan':
                             
                             ad = aa[1]
@@ -136,91 +137,91 @@ class twscan_contour:
                             ap = aa[1]
                         
                         else:
-                            print('neteTSplot_method, please check scan_style')
-                    
-                    else:
-                        ad = aa
+                            print('twscan_contour_plot, please check scan_style')
+                            
                         
-                    
-            
-                    if scan_style == 'denscan':
-                        
-                        fig, axs = plt.subplots()
-                        
-                        title_ap = float(ap)*pow(10, 5)
-                        label_ad = float(ad)*pow(10, 20)
-                        
-                        nf = aa[0]
-                        tf = aa[1]
-                        
-                        
+                
+                        if scan_style == 'denscan':
+                            
+                            fig, axs = plt.subplots()
+                            
+                            title_ap = float(ap)*pow(10, 5)
+                            label_ad = float(ad)*pow(10, 20)
+                            
+                            nf = aa[0]
+                            tf = aa[1]
+                            
+                            
 
-                        b2fstate = self.data['b2fstate'][nf][tf]
-                        ne_dat = b2fstate['ne'][1:nx+1, 1:ny+1]
-                        Te_J = b2fstate['te'][1:nx+1, 1:ny+1]
-                        
-                        ev = 1.6021766339999999 * pow(10, -19)
-                        te_dat = Te_J / ev
-                                        
-                        psi_coord = self.data['psi']['psival'][1:ny+1, 1:nx+1]
-                        
-                        source = self.data['b2wdat'][nf][tf]['b2npc_sna'][0][1:nx+1, 1:ny+1]                
-                        vol = self.data['b2wdat'][nf][tf]['vol'][1:nx+1, 1:ny+1]
-                        sx = np.divide(source, vol)
-                        
-                        neuden_dat = self.data['ft44'][nf][tf]['dab2'][:, :, 0]
-                        
-                        vessel = self.data['vessel']
-                        self.twcontourp(plot_2dval = te_dat, R_coord = RadLoc, Z_coord = VertLoc, 
-                                          quantity = f'{aa} Electron temperature', axs = axs, 
-                                          norm = 'lnorm', lv = 40)
-                        
-                        axs.plot(vessel[:,0]/1000, vessel[:,1]/1000, color = 'black')
-                        
+                            b2fstate = self.data['b2fstate'][nf][tf]
+                            ne_dat = b2fstate['ne'][1:nx+1, 1:ny+1]
+                            Te_J = b2fstate['te'][1:nx+1, 1:ny+1]
+                            
+                            ev = 1.6021766339999999 * pow(10, -19)
+                            te_dat = Te_J / ev
+                                            
+                            psi_coord = self.data['psi']['psival'][1:ny+1, 1:nx+1]
+                            
+                            source = self.data['b2wdat'][nf][tf]['b2npc_sna'][0][1:nx+1, 1:ny+1]                
+                            vol = self.data['b2wdat'][nf][tf]['vol'][1:nx+1, 1:ny+1]
+                            sx = np.divide(source, vol)
+                            
+                            neuden_dat = self.data['ft44'][nf][tf]['dab2'][:, :, 0]
+                            
+                            vessel = self.data['vessel']
+                            self.twcontourp(plot_2dval = te_dat, R_coord = RadLoc, Z_coord = VertLoc, 
+                                              quantity = f'{aa} Electron temperature', axs = axs, 
+                                              norm = 'lnorm', lv = 40)
+                            
+                            axs.plot(vessel[:,0]/1000, vessel[:,1]/1000, color = 'black')
+                            
+
+                                    
+                            axs.set_xlim(0, 2)
+                            axs.set_ylim(-2, -1)
+                            
+                            
+                            
+                
+
+                        elif scan_style == 'tempscan':
+                            
+                            title_ap = float(ap)*pow(10, 20)
+                            label_ad = float(ad)*pow(10, 5)
+                            # exp_an_fit = fit_dat['exp_fit']
+                            # xcoord_cut = fit_dat['x_coord_cut']
 
                                 
-                        axs.set_xlim(0, 2)
-                        axs.set_ylim(-2, -1)
-                        
-                        
-                        
-            
-
-                    elif scan_style == 'tempscan':
-                        
-                        title_ap = float(ap)*pow(10, 20)
-                        label_ad = float(ad)*pow(10, 5)
-                        # exp_an_fit = fit_dat['exp_fit']
-                        # xcoord_cut = fit_dat['x_coord_cut']
-
+                            nf = aa[0]
+                            tf = aa[1]
                             
-                        nf = aa[0]
-                        tf = aa[1]
-                        
 
 
-                        b2fstate = self.data['b2fstate'][nf][tf]
-                        ne_dat = b2fstate['ne'][1:nx+1, 1:ny+1]
-                        Te_J = b2fstate['te'][1:nx+1, 1:ny+1]
-                        
-                        ev = 1.6021766339999999 * pow(10, -19)
-                        te_dat = Te_J / ev
-                                        
-                        psi_coord = self.data['psi']['psival'][1:ny+1, 1:nx+1]
-                        
-                        source = self.data['b2wdat'][nf][tf]['b2npc_sna'][0][1:nx+1, 1:ny+1]                
-                        vol = self.data['b2wdat'][nf][tf]['vol'][1:nx+1, 1:ny+1]
-                        sx = np.divide(source, vol)
-                        
-                        neuden_dat = self.data['ft44'][nf][tf]['dab2'][:, :, 0]
-                        
-                        
-                        self.contour_plot(plot_2dval = te_dat, R_coord = RadLoc, Z_coord = VertLoc, 
-                                          quantity = f'{aa[0]:.2f/aa[1]:.2f} Electron temperature')
+                            b2fstate = self.data['b2fstate'][nf][tf]
+                            ne_dat = b2fstate['ne'][1:nx+1, 1:ny+1]
+                            Te_J = b2fstate['te'][1:nx+1, 1:ny+1]
+                            
+                            ev = 1.6021766339999999 * pow(10, -19)
+                            te_dat = Te_J / ev
+                                            
+                            psi_coord = self.data['psi']['psival'][1:ny+1, 1:nx+1]
+                            
+                            source = self.data['b2wdat'][nf][tf]['b2npc_sna'][0][1:nx+1, 1:ny+1]                
+                            vol = self.data['b2wdat'][nf][tf]['vol'][1:nx+1, 1:ny+1]
+                            sx = np.divide(source, vol)
+                            
+                            neuden_dat = self.data['ft44'][nf][tf]['dab2'][:, :, 0]
                             
                             
-                    else:
-                        print('neteTSplot_structure, please check the scan parameter')
+                            self.contour_plot(plot_2dval = te_dat, R_coord = RadLoc, Z_coord = VertLoc, 
+                                              quantity = f'{aa[0]:.2f/aa[1]:.2f} Electron temperature')
+                                
+                                
+                        else:
+                            print('neteTSplot_structure, please check the scan parameter')
+        
+        
+        
                 
                 
                 
