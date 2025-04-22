2174a88819ca0d48be030fa79938a1ccbd1667b2
Yi-Cheng Chuang <ychuang@wm.edu>
2025-04-22 14:53:56 -0400

    Create SOLPSplotter_datareorder_dr.py

---
+++ old_drs/SOLPSplotter_datareorder_dr.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 17:29:22 2024
+
+@author: ychuang
+"""
+
+import SOLPS_set as sps
+import SOLPSplotter_datareorder as sdr
+import SOLPS_transcoe_adj as sta
+
+d = sps.Setting_dic()
+lex = sps.loadDS_dic(d['DEV'])
+
+
+
+xl = sdr.data_reorder(DefaultSettings = d, loadDS = lex)
+
+xl.load_mast_dir()
+xl.load_solpsgeo()
+# xl.calcpsi()
+xl.calcpsi_avcr()
+xl.calc_RRsep(plotRR= False, plot_psi_dsa_align= False)
+fitmastexp_setting_dic = {'writefile': True, 'plot_solps_fit': False, 
+                          'plot_exp_and_fit': False, 'plot_shift_compare': False,
+                          'data_print': True}
+xl.fitmastexp(plot_setting_dic = fitmastexp_setting_dic)
+xl.load_b2fstate()
+xl.load_b2fplasmf()
+xl.load_output_data(param= 'Te')
+xl.calc_sep_dsa()
+
+
+poloidal_index_list = ['59']
+xl.calc_dsa(pol_loc= poloidal_index_list[0])
+
+
+
+xl.set_plot()
+poloidal_index_list = []
+for i in range(47):
+    poloidal_index_list.append('{}'.format(25 + i))
+    
+xl.opacity_data_fit(pol_list = poloidal_index_list)
+xl.calc_pol_angle(pol_list = poloidal_index_list, plot_angle = False)
+
+xl.plot_data_reorder(pol_list = poloidal_index_list)
