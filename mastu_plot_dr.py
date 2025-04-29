4d88c45ebc64f46748ed8f54bfee61a40048dd6a
Yi-Cheng Chuang <ychuang@wm.edu>
2025-04-29 00:53:17 -0400

    Update mastu_plot_dr.py

--- mastu_plot_dr.py
+++ mastu_plot_dr.py
@@ -7,7 +7,9 @@ Created on Tue Apr 22 15:06:54 2025
 
 from SOLPS_input.input_setting import Setting_dic, loadDS_dic
 from module_load_for_plot.mastu_module_load import *
-
+from plot_geometry.SOLPSplotter_plotgeo import plot_geo
+from plot_geometry.plan_step import step_planning
+from contour_plot.B2_boundary_plot import B2_boundary_contour
 
 
 
@@ -33,6 +35,10 @@ class mastu_datapipline:
     def run_mastu_plot(self):
         
         xml = mastu_load_prepare_module(DF = self.DF, data = self.data)
+        xpg = plot_geo(DF = self.DF, data = self.data)
+        xsp = step_planning(DF = self.DF, data = self.data)
+        xbbc = B2_boundary_contour(DF = self.DF, data = self.data)
+
         
         
         
@@ -42,9 +48,24 @@ class mastu_datapipline:
             xml.mastu_prepare_for_plots()
             
             
+            plot_theme = 'step_plan'
             
+            if plot_theme == 'geo':
+                
+                             
+                xpg.plot_g()
+                xpg.plot_sec()
             
+            elif plot_theme == 'step_plan':
+                
+                xsp.mastu_Ra_measure()
+                xsp.vessel_change(plan_major_radius = 3.61, plan_minor_radius = 2)
             
+            elif plot_theme == 'B2_boundary':
+                
+                
+                xbbc.plot_B2boundary()
+                
             
 
 
