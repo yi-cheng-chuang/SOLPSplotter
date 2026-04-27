a688bf26e12a63b768574ca5f9409fcadb1d513c
Yi-Cheng Chuang <ychuang@wm.edu>
2026-04-27 11:16:58 -0400

    Update series_Smid.py

--- radial_plot/series_Smid.py
+++ radial_plot/series_Smid.py
@@ -11,14 +11,19 @@ import statistics
 import matplotlib.pyplot as plt
 from scipy import interpolate
 import matplotlib.pylab as pylab
+from scipy.optimize import curve_fit
+from fit_data.fitting_method import fit_method_collection
+from radial_plot.SOLPSplotter_NTplot import NT_plot
 
 
 class series_midS:
 
-    def __init__(self, DF, data):
+    def __init__(self, DF, data, fmc: fit_method_collection, ntp: NT_plot):
 
         self.DF = DF
         self.data = data
+        self.fmc = fmc
+        self.ntp = ntp
 
     def Splot_method(self, iterlist, cl_dic, xcoord_type):
 
@@ -38,6 +43,25 @@ class series_midS:
         dat_struc = {'nx': nx, 'ny': ny}
 
         avg_te_list = []
+        fit_exp_dic = {}
+        te_range_list = []
+        te_range_list.append(
+            round(self.data['opacity_poloidal']['4.5']['electron_pedestal_temperature'][0], 2))
+        te_range_list.append(
+            round(self.data['opacity_poloidal']['4.5']['electron_temperature_separatrix'][0], 2))
+        tie_range_list = []
+        tie_range_list.append(
+            round(self.data['midplane_profile']['4.5']['mid_ti'][0], 2))
+        tie_range_list.append(
+            round(self.data['midplane_profile']['4.5']['mid_te'][0], 2))
+
+        TS_dic = self.ntp.plot_neteTSdat()
+
+        psi = TS_dic['psi']
+        exp_ne = TS_dic['neTS']
+        ne_er = TS_dic['errne']
+        exp_te = TS_dic['teTS']
+        te_er = TS_dic['errte']
 
         for aa in iterlist:
 
@@ -108,13 +132,55 @@ class series_midS:
 
             molsource = self.data['ft44'][aa]['srcml'][59, :, 0]
 
+            exp_fit = False
             rrsep_solps = psi_to_dsa_func(psi_coord)
 
-            # avg_te =
-            # avg_te_list.append()
+            if exp_fit:
+
+                index_max = np.where(norm_S == 1)[0][0]
+                print("This is index max:")
+                print(index_max)
+
+                inside_peak_list = norm_S[:index_max]
+
+                pn = [1.0, 200.5]
+                x_sh = rrsep_solps[:index_max]
+                popt_an, pcov_an = curve_fit(
+                    self.fmc.expfit, x_sh, inside_peak_list, pn)
+                # print(popt_an)
+
+                exp_an_fit = self.fmc.expfit(
+                    x_sh, popt_an[0], popt_an[1])
+
+                plt.figure()
+                plt.yscale('log')
+                plt.plot(x_sh, inside_peak_list, 'o-',
+                         color='green', label='norm source')
+                plt.plot(x_sh, exp_an_fit, color='r', lw=5,
+                         label='norm source exponential fit')
+                plt.xlabel('rrsep')
+                plt.ylabel('Source rate $(m^{-3}/s)$')
+                plt.title('Normalize source rate with fits')
+                plt.legend()
+
+                fit_exp_dic[aa] = {
+                    'exp_an_fit': exp_an_fit, 'popt_an': popt_an}
+
+            te_ped = self.data['opacity_poloidal'][aa]['electron_pedestal_temperature'][0]
+            te_sep = self.data['opacity_poloidal'][aa]['electron_temperature_separatrix'][0]
+
+            avg_te = 0.5 * (te_ped + te_sep)
+            avg_te_list.append(round(avg_te, 2))
 
             trim = False
-            dat = S_norm_inc_dat
+
+            dat_name = 'gradn'
+
+            if dat_name == 'ne':
+                dat = mid_ne_pro
+
+            elif dat_name == 'te':
+                dat = mid_te_pro
 
             if xcoord_type == 'psi':
 
@@ -124,9 +190,28 @@ class series_midS:
                              color=cl_dic[aa], label='{}'.format(aa))
 
                 else:
+                    if dat_name == 'ne':
 
-                    axs.plot(psi_coord, dat, lins_map[aa] + '-', lw=1.5,
-                             color=cl_dic[aa], label='{}'.format(aa))
+                        axs.plot(psi_coord, dat, lins_map[aa] + '-', lw=1.5,
+                                 color=cl_dic[aa], label='{}'.format(aa))
+
+                    elif dat_name == 'te':
+
+                        axs.plot(psi_coord, dat, lins_map[aa] + '-', lw=1.5,
+                                 color=cl_dic[aa], label='{}'.format(aa))
+
+                    elif dat_name == 'gradn':
+
+                        # Compute gradient dn/dr
+                        dn_dr = np.gradient(mid_ne_pro, psi_coord)
+
+                        axs.plot(psi_coord, dn_dr, lins_map[aa] + '-', lw=1.5,
+                                 color=cl_dic[aa], label='{}'.format(aa))
+
+                    else:
+
+                        axs.plot(psi_coord, dat, lins_map[aa] + '-', lw=1.5,
+                                 color=cl_dic[aa], label='{}'.format(aa))
 
             elif xcoord_type == 'rrsep':
 
@@ -134,6 +219,14 @@ class series_midS:
 
                     axs.plot(rrsep_solps[:33], dat, lins_map[aa] + '-', lw=1.5, color=cl_dic[aa],
                              label='{}'.format(aa))
+
+                elif dat_name == 'gradn':
+
+                    # Compute gradient dn/dr
+                    dn_dr = np.gradient(mid_ne_pro, rrsep_solps)
+
+                    axs.plot(rrsep_solps, dn_dr, lins_map[aa] + '-', lw=1.5,
+                             color=cl_dic[aa], label='{}'.format(aa))
 
                 else:
 
@@ -143,15 +236,35 @@ class series_midS:
         # axs.set_title('midS')
         # axs.set_yscale('log')
         # axs.set_ylim(50, 175)
-        axs.set_xlim(-0.02, 0.01)
+        # axs.set_xlim(-0.02, 0.01)
         # axs.set_xlim(left=-0.02)
         # axs.set_ylim(0.05, 1.2)
         # axs.set_yscale("log")
         # axs.set_ylim(4E+21, 3E+23)
-        axs.axvline(x=max(psi_to_dsa_func(xcoord_cut)), color='black', lw=3, ls='--',
-                    label='pedestal $\Delta n_e$')
-        axs.axvline(x=min(psi_to_dsa_func(xcoord_cut)),
-                    color='black', lw=3, ls='--')
+
+        if dat_name == 'ne':
+
+            axs.errorbar(psi, exp_ne, yerr=ne_er, fmt='o',
+                         color='purple', label='$n_e$ TS data')
+
+        elif dat_name == 'te':
+
+            axs.errorbar(psi, exp_te, yerr=te_er, fmt='o',
+                         color='purple', label='$t_e$ TS data')
+        if xcoord_type == 'psi':
+
+            axs.axvline(x=max(xcoord_cut), color='black', lw=3, ls='--',
+                        label='pedestal $\Delta n_e$')
+            axs.axvline(x=min(xcoord_cut),
+                        color='black', lw=3, ls='--')
+
+        elif xcoord_type == 'rrsep':
+
+            axs.axvline(x=max(psi_to_dsa_func(xcoord_cut)), color='black', lw=3, ls='--',
+                        label='pedestal $\Delta n_e$')
+            axs.axvline(x=min(psi_to_dsa_func(xcoord_cut)),
+                        color='black', lw=3, ls='--')
+
         if xcoord_type == 'psi':
 
             axs.set_xlabel('$\psi_N$')
@@ -161,6 +274,13 @@ class series_midS:
 
         axs.grid(True)
         axs.legend()
+
+        print("average te is:")
+        print(avg_te_list)
+        print('te range list:')
+        print(te_range_list)
+        print('tie range list:')
+        print(tie_range_list)
 
     def srmidS_plot(self, xcoord_type):
 
