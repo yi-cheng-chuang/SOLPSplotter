f4cb904f2d5137b30cbe5f37acc4e2b91e6649ee
Yi-Cheng Chuang <ychuang@wm.edu>
2026-04-27 11:16:12 -0400

    Update arclength_map_generator.py

--- midplane_data/arclength_map_generator.py
+++ midplane_data/arclength_map_generator.py
@@ -21,24 +21,64 @@ class arc_length_measure:
 
     def arc_length_map(self, pol_list):
 
-        nx = self.data['b2fgeo']['nx']
-        ny = self.data['b2fgeo']['ny']
+        withshift = self.DF.withshift
+        withseries = self.DF.withseries
 
-        RadLoc = np.transpose(self.data['grid']['RadLoc'])[1:nx + 1, 1:ny + 1]
-        VertLoc = np.transpose(self.data['grid']['VertLoc'])[
-            1:nx + 1, 1:ny + 1]
+        if withshift == True and withseries == False:
 
-        pt = len(pol_list)
-        arc_map = np.zeros((pt, 36))
+            arc_map_dic = {}
 
-        for it, aa in enumerate(pol_list):
+            for aa in self.data['dircomp']['Attempt'].keys():
 
-            rline = RadLoc[int(aa), :]
-            zline = VertLoc[int(aa), :]
+                nx = self.data['b2fgeo'][aa]['nx']
+                ny = self.data['b2fgeo'][aa]['ny']
 
-            arclength, inp = self.rp.RR_sep_calculator(cr=rline, cz=zline)
-            arc_arcsep = arclength - np.mean([arclength[18], arclength[19]])
+                RadLoc = np.transpose(self.data['grid']['RadLoc'][aa])[
+                    1:nx + 1, 1:ny + 1]
+                VertLoc = np.transpose(self.data['grid']['VertLoc'][aa])[
+                    1:nx + 1, 1:ny + 1]
 
-            arc_map[it, :] = arc_arcsep
+                pt = len(pol_list)
+                arc_map = np.zeros((pt, 36))
 
-        self.data['arc_map'] = arc_map
+                for it, bb in enumerate(pol_list):
+
+                    rline = RadLoc[int(bb), :]
+                    zline = VertLoc[int(bb), :]
+
+                    arclength, inp = self.rp.RR_sep_calculator(
+                        cr=rline, cz=zline)
+                    arc_arcsep = arclength - \
+                        np.mean([arclength[18], arclength[19]])
+
+                    arc_map[it, :] = arc_arcsep
+
+                arc_map_dic[aa] = arc_map
+
+            self.data['arc_map'] = arc_map_dic
+
+        else:
+
+            nx = self.data['b2fgeo']['nx']
+            ny = self.data['b2fgeo']['ny']
+
+            RadLoc = np.transpose(self.data['grid']['RadLoc'])[
+                1:nx + 1, 1:ny + 1]
+            VertLoc = np.transpose(self.data['grid']['VertLoc'])[
+                1:nx + 1, 1:ny + 1]
+
+            pt = len(pol_list)
+            arc_map = np.zeros((pt, 36))
+
+            for it, aa in enumerate(pol_list):
+
+                rline = RadLoc[int(aa), :]
+                zline = VertLoc[int(aa), :]
+
+                arclength, inp = self.rp.RR_sep_calculator(cr=rline, cz=zline)
+                arc_arcsep = arclength - \
+                    np.mean([arclength[18], arclength[19]])
+
+                arc_map[it, :] = arc_arcsep
+
+            self.data['arc_map'] = arc_map
